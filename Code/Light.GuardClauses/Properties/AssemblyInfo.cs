d3989a6c4604c97b760066b00d119633eebd5720
Kenny Pflug <kenny.pflug@live.de>
2016-03-19 12:18:23 +0100

    removed unnecessary attributes in AssemblyInfo

--- Code/Light.GuardClauses/Properties/AssemblyInfo.cs
+++ Code/Light.GuardClauses/Properties/AssemblyInfo.cs
@@ -1,14 +1,9 @@
 ﻿using System.Reflection;
-using System.Resources;
 
 [assembly: AssemblyTitle("Light.GuardClauses")]
 [assembly: AssemblyDescription("A lightsweight library for expressive Guard Clauses with conditional compilation in C#.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kenny Pflug")]
 [assembly: AssemblyProduct("Light.GuardClauses")]
 [assembly: AssemblyCopyright("Copyright © 2016 Kenny Pflug")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 [assembly: AssemblyVersion("0.1.0")]
