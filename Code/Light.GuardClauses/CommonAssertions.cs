623730c3ac686621a3327244bfda410abbd3917c
Kenny Pflug <kenny.pflug@live.de>
2016-03-22 11:30:52 +0100

    XML comment reformatting for CommonAssertions

--- Code/Light.GuardClauses/CommonAssertions.cs
+++ Code/Light.GuardClauses/CommonAssertions.cs
@@ -5,27 +5,24 @@ using Light.GuardClauses.Exceptions;
 namespace Light.GuardClauses
 {
     /// <summary>
-    ///     This class contains the most common assertions like MustNotBeNull and assertions that are not directly related to
-    ///     categories like collection assertions or string assertions.
+    ///     This class contains the most common assertions like <see cref="MustNotBeNull{T}" /> and assertions that are not directly related to
+    ///     any categories like collection assertions or string assertions.
     /// </summary>
     public static class CommonAssertions
     {
         /// <summary>
-        ///     Ensures that the specified parameter is not <c>null</c>, or otherwise throws an
-        ///     <see cref="ArgumentNullException" />.
+        ///     Ensures that the specified parameter is not <c>null</c>, or otherwise throws an <see cref="ArgumentNullException" />.
         /// </summary>
         /// <typeparam name="T">The type of the parameter to be checked.</typeparam>
         /// <param name="parameter">The parameter to be checked.</param>
         /// <param name="parameterName">The name of the parameter (optional).</param>
         /// <param name="message">The message that will be injected into the <see cref="ArgumentNullException" /> (optional).</param>
         /// <param name="exception">
-        ///     The exception that is thrown when the specified <paramref name="parameter" /> is <c>null</c> (optional). Please
-        ///     note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify
-        ///     exception.
+        ///     The exception that is thrown when the specified <paramref name="parameter" /> is <c>null</c> (optional).
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="ArgumentNullException">
-        ///     Thrown when the specified parameter is <c>null</c> and no
-        ///     <paramref name="exception" /> was specified.
+        ///     Thrown when the specified parameter is <c>null</c> and no <paramref name="exception" /> is specified.
         /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustNotBeNull<T>(this T parameter, string parameterName = null, string message = null, Exception exception = null) where T : class
@@ -35,24 +32,20 @@ namespace Light.GuardClauses
         }
 
         /// <summary>
-        ///     Ensures that the specified parameter is <c>null</c>, or otherwise throws an <see cref="ArgumentNotNullException" />
-        ///     .
+        ///     Ensures that the specified parameter is <c>null</c>, or otherwise throws an <see cref="ArgumentNotNullException" />.
         /// </summary>
         /// <typeparam name="T">The type of the parameter to be checked.</typeparam>
         /// <param name="parameter">The parameter to be checked.</param>
         /// <param name="parameterName">The name of the parameter (optional).</param>
         /// <param name="message">
         ///     The message that will be injected into the <see cref="ArgumentNotNullException" /> (optional).
-        ///     Please note that <paramref name="parameterName" /> is ignored when you use message.
         /// </param>
         /// <param name="exception">
-        ///     The exception that is thrown when the specified <paramref name="parameter" /> is not <c>null</c> (optional). Please
-        ///     note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify
-        ///     exception.
+        ///     The exception that is thrown when the specified <paramref name="parameter" /> is not <c>null</c> (optional).
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="ArgumentNotNullException">
-        ///     Thrown when the specified parameter is not <c>null</c> and no
-        ///     <paramref name="exception" /> was specified.
+        ///     Thrown when the specified parameter is not <c>null</c> and no <paramref name="exception" /> is specified.
         /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustBeNull<T>(this T parameter, string parameterName = null, string message = null, Exception exception = null) where T : class
@@ -62,31 +55,27 @@ namespace Light.GuardClauses
         }
 
         /// <summary>
-        ///     Ensures that parameter is of the specified type and returns the downcasted value, or otherwise throws a
-        ///     <see cref="TypeMismatchException" />.
+        ///     Ensures that parameter is of the specified type and returns the downcasted value, or otherwise throws a <see cref="TypeMismatchException" />.
         /// </summary>
         /// <typeparam name="T">The type of the parameter to be checked.</typeparam>
         /// <param name="parameter">The parameter to be checked.</param>
         /// <param name="parameterName">The name of the parameter (optional).</param>
         /// <param name="message">
         ///     The message that will be injected into the <see cref="TypeMismatchException" /> (optional).
-        ///     Please note that <paramref name="parameterName" /> is ignored when you use message.
         /// </param>
         /// <param name="exception">
         ///     The exception that is thrown when the specified <paramref name="parameter" /> cannot be downcasted (optional).
-        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you
-        ///     specify exception.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="TypeMismatchException">
-        ///     Thrown when the specified <paramref name="parameter" /> cannot be downcasted and no
-        ///     <paramref name="exception" /> was specified.
+        ///     Thrown when the specified <paramref name="parameter" /> cannot be downcasted and no <paramref name="exception" /> is specified.
         /// </exception>
         /// <returns>The downcasted reference to <paramref name="parameter" />.</returns>
         public static T MustBeType<T>(this object parameter, string parameterName = null, string message = null, Exception exception = null) where T : class
         {
             var castedValue = parameter as T;
             if (castedValue == null)
-                throw exception ?? new TypeMismatchException(message ?? $"{parameterName ?? "The object"} is of type {parameter.GetType().FullName} and cannot be downcasted to {typeof(T).FullName}.", parameterName);
+                throw exception ?? new TypeMismatchException(message ?? $"{parameterName ?? "The object"} is of type {parameter.GetType().FullName} and cannot be downcasted to {typeof (T).FullName}.", parameterName);
 
             return castedValue;
         }
@@ -99,16 +88,13 @@ namespace Light.GuardClauses
         /// <param name="parameterName">The name of the parameter (optional).</param>
         /// <param name="message">
         ///     The message that will be injected into the <see cref="NullableHasNoValueException" /> (optional).
-        ///     Please note that <paramref name="parameterName" /> is ignored when you use message.
         /// </param>
         /// <param name="exception">
         ///     The exception that is thrown when the specified Nullable has no value (optional).
-        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you
-        ///     specify exception.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="NullableHasNoValueException">
-        ///     Thrown when the specified nullable has no value and no
-        ///     <paramref name="exception" /> was specified.
+        ///     Thrown when the specified nullable has no value and no <paramref name="exception" /> is specified.
         /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustHaveValue<T>(this T? parameter, string parameterName = null, string message = null, Exception exception = null) where T : struct
@@ -125,16 +111,13 @@ namespace Light.GuardClauses
         /// <param name="parameterName">The name of the parameter (optional).</param>
         /// <param name="message">
         ///     The message that will be injected into the <see cref="NullableHasValueException" /> (optional).
-        ///     Please note that <paramref name="parameterName" /> is ignored when you use message.
         /// </param>
         /// <param name="exception">
         ///     The exception that is thrown when the specified Nullable has a value (optional).
-        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you
-        ///     specify exception.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="NullableHasValueException">
-        ///     Thrown when the specified nullable has a value and no
-        ///     <paramref name="exception" /> was specified.
+        ///     Thrown when the specified nullable has a value and no <paramref name="exception" /> is specified.
         /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustNotHaveValue<T>(this T? parameter, string parameterName = null, string message = null, Exception exception = null) where T : struct
@@ -144,24 +127,20 @@ namespace Light.GuardClauses
         }
 
         /// <summary>
-        ///     Ensures that the specified value is defined in its corresponding enum type, or otherwise throws a
-        ///     <see cref="EnumValueNotDefinedException" />.
+        ///     Ensures that the specified value is defined in its corresponding enum type, or otherwise throws a <see cref="EnumValueNotDefinedException" />.
         /// </summary>
         /// <typeparam name="T">The enum type of the parameter.</typeparam>
         /// <param name="parameter">The parameter to be checked.</param>
         /// <param name="parameterName">The name of the parameter (optional).</param>
         /// <param name="message">
         ///     The message that will be injected into the <see cref="EnumValueNotDefinedException" /> (optional).
-        ///     Please note that <paramref name="parameterName" /> is ignored when you use message.
         /// </param>
         /// <param name="exception">
         ///     The exception that is thrown when the specified Nullable has a value (optional).
-        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you
-        ///     specify exception.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="EnumValueNotDefinedException">
-        ///     Thrown when the specified enum value is not defined and no
-        ///     <paramref name="exception" /> was specified.
+        ///     Thrown when the specified enum value is not defined and no <paramref name="exception" /> is specified.
         /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustBeValidEnumValue<T>(this T parameter, string parameterName = null, string message = null, Exception exception = null)
@@ -181,12 +160,10 @@ namespace Light.GuardClauses
         /// </param>
         /// <param name="exception">
         ///     The exception that is thrown when the specified GUID is empty (optional).
-        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you
-        ///     specify exception.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
         /// </param>
         /// <exception cref="EmptyGuidException">
-        ///     Thrown when the specified GUID is empty and no <paramref name="exception" /> was
-        ///     specified.
+        ///     Thrown when the specified GUID is empty and no <paramref name="exception" /> is specified.
         /// </exception>
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustNotBeEmpty(this Guid parameter, string parameterName = null, string message = null, Exception exception = null)
