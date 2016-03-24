044cd4e65d6e6aa541fbda751101fb5666be8e0e
Kenny Pflug <kenny.pflug@live.de>
2016-03-24 11:42:33 +0100

    added MustBeSameAs

---
+++ Code/Light.GuardClauses.Tests/MustBeSameAsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests
+{
+    public sealed class MustBeSameAsTests
+    {
+        [Theory(DisplayName = "MustBeSameAs must throw an ArgumentException when the two specified references do not point to the same instance.")]
+        [InlineData("Hello", "World")]
+        [InlineData("1", "2")]
+        [InlineData(new object[] { }, new object[] { "Foo" })]
+        public void ReferencesDifferent<T>(T first, T second) where T : class
+        {
+            Action act = () => first.MustBeSameAs(second, nameof(first));
+
+            act.ShouldThrow<ArgumentException>()
+               .And.Message.Should().Contain($"{nameof(first)} must point to the same object instance as \"{second}\", but it does not.");
+        }
+
+        [Theory(DisplayName = "MustBeSameAs must not throw an exception when the two specified references point to the same instance.")]
+        [InlineData("Foo")]
+        [InlineData("Bar")]
+        public void ReferencesEqual<T>(T value) where T : class
+        {
+            Action act = () => value.MustBeSameAs(value);
+
+            act.ShouldNotThrow();
+        }
+
+        [Fact(DisplayName = "The caller can specify a custom message that MustBeSameAs must inject instead of the default one.")]
+        public void CustomMessage()
+        {
+            const string message = "Thou shall be one and the same!";
+
+            Action act = () => "foo".MustBeSameAs("bar", message: message);
+
+            act.ShouldThrow<ArgumentException>()
+               .And.Message.Should().Contain(message);
+        }
+
+        [Fact(DisplayName = "The caller can specify a custom exception that MustBeSameAs must raise instead of the default one.")]
+        public void CustomException()
+        {
+            var exception = new Exception();
+
+            Action act = () => "foo".MustBeSameAs("bar", exception: exception);
+
+            act.ShouldThrow<Exception>().Which.Should().BeSameAs(exception);
+        }
+    }
+}
