1c97e1fe7b6b3ec8f63c0775b8213f05e371a600
Kenny Pflug <kenny.pflug@live.de>
2016-06-02 21:26:03 +0200

    Added PowerShell script that removes the COMPILE_ASSERTIONS symbol when the Nuget Package is uninstalled

---
+++ Code/NuGet.PowershellScripts/uninstall.ps1
@@ -0,0 +1,46 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+# Constants and Variables
+$condiditionalCompilationSymbolsId = "DefineConstants"
+$compileAssertionsSymbol = "COMPILE_ASSERTIONS"
+$shouldSave = $false
+
+# This function removes all instances of COMPILE_ASSERTIONS from the given conditional compilation symbols
+function Remove-CompileAssertionsSymbol([string]$conditionalCompilationSymbols)
+{
+    if ($conditionalCompilationSymbols.Length -eq 0)
+    {
+        return $conditionalCompilationSymbols
+    }
+
+    if ($conditionalCompilationSymbols -eq $compileAssertionsSymbol)
+    {
+        return ""
+    }
+
+    $conditionalCompilationSymbols = $conditionalCompilationSymbols -replace "$compileAssertionsSymbol;", ""
+    $conditionalCompilationSymbols = $conditionalCompilationSymbols -replace ";$compileAssertionsSymbol", ""
+    return $conditionalCompilationSymbols
+}
+
+# Run through all the configurations (Debug, Release, and so on) and remove the COMPILE_ASSERTIONS symbol
+foreach ($configuration in $project.ConfigurationManager)
+{
+    $conditionalCompilationSymbols = $configuration.Properties.Item($condiditionalCompilationSymbolsId).Value
+
+    $newConditionalCompilationSymbols = Remove-CompileAssertionsSymbol($conditionalCompilationSymbols)
+
+    # If the conditional compilation symbols changed, then set them on the configuration and remember to save the project
+    if ($condiditionalCompilationSymbols -ne $newConditionalCompilationSymbols)
+    {
+        $shouldSave = $true
+
+        $configuration.Properties.Item($condiditionalCompilationSymbolsId).Value = $newConditionalCompilationSymbols
+    }
+}
+
+if ($shouldSave)
+{
+    $project.Save()
+    Write-Host "Light.GuardClauses: $compileAssertionsSymbol removed"
+}
