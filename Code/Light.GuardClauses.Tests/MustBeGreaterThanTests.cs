d330bbfc72a84f0d66c874f6dc5862542a37e88f
Kenny Pflug <kenny.pflug@live.de>
2016-06-12 09:56:59 +0200

    added MustBeGreaterThan

---
+++ Code/Light.GuardClauses.Tests/MustBeGreaterThanTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Tests.CustomMessagesAndExceptions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests
+{
+    public sealed class MustBeGreaterThanTests : ICustomMessageAndExceptionTestDataProvider
+    {
+        [Theory(DisplayName = "MustBeGreaterThan must throw an ArgumentOutOfRangeException when the boundary value is greater or equal to the parameter value to be checked.")]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(-1, -1)]
+        [InlineData(0, 0)]
+        [InlineData(short.MinValue, short.MinValue)]
+        [InlineData('F', 'c')]
+        [InlineData("aaa", "bbb")]
+        public void ParameterEqualOrLess<T>(T first, T second) where T : IComparable<T>
+        {
+            Action act = () => first.MustBeGreaterThan(second, nameof(first));
+
+            act.ShouldThrow<ArgumentOutOfRangeException>()
+               .And.Message.Should().Contain($"{nameof(first)} must be greater than {second}, but you specified {first}.");
+        }
+
+        [Theory(DisplayName = "MustBeGreaterThan must not throw an exception when the specified parameter is greater than the boundary value.")]
+        [InlineData(133, 99)]
+        [InlineData(int.MaxValue, int.MaxValue - 1)]
+        [InlineData(byte.MaxValue, byte.MinValue)]
+        [InlineData('Z', 'X')]
+        [InlineData("Hello", "ah yeah")]
+        public void ParameterGreater<T>(T first, T second) where T : IComparable<T>
+        {
+            Action act = () => first.MustBeGreaterThan(second);
+
+            act.ShouldNotThrow();
+        }
+
+        public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
+        {
+            testData.Add(new CustomExceptionTest(exception => 1.MustBeGreaterThan(2, exception: exception)))
+                    .Add(new CustomMessageTest<ArgumentOutOfRangeException>(message => 42.MustBeGreaterThan(87, message: message)));
+        }
+    }
+}
--- Code/Light.GuardClauses/ComparableAssertions.cs
+++ Code/Light.GuardClauses/ComparableAssertions.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 namespace Light.GuardClauses
 {
     /// <summary>
-    ///     The <see cref="ComparableAssertions"/> class contains extension methods that check assertions for the <see cref="IComparable{T}" /> interface.
+    ///     The <see cref="ComparableAssertions" /> class contains extension methods that check assertions for the <see cref="IComparable{T}" /> interface.
     /// </summary>
     public static class ComparableAssertions
     {
@@ -78,6 +78,28 @@ namespace Light.GuardClauses
         {
             if (parameter.CompareTo(boundary) > 0)
                 throw exception != null ? exception() : new ArgumentOutOfRangeException(parameterName, parameter, message ?? $"{parameterName ?? "The value"} must not be greater than {boundary}, but you specified {parameter}.");
+        }
+
+        /// <summary>
+        ///     Ensures that the specified <paramref name="parameter" /> is greater than the given <paramref name="boundary" /> value, or otherwise throws an <see cref="ArgumentOutOfRangeException" />.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter to be checked.</typeparam>
+        /// <param name="parameter">The parameter to be checked.</param>
+        /// <param name="boundary">The boundary value that <paramref name="parameter" /> must be greater than.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message that should be injected into the <see cref="ArgumentOutOfRangeException" /> (optional).</param>
+        /// <param name="exception">
+        ///     The exception that is thrown when the specified <paramref name="parameter" /> is not greater than <paramref name="boundary" /> (optional).
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when the specified <paramref name="parameter" /> is not greater than <paramref name="boundary" /> and no <paramref name="exception" /> is specified.
+        /// </exception>
+        [Conditional(Check.CompileAssertionsSymbol)]
+        public static void MustBeGreaterThan<T>(this T parameter, T boundary, string parameterName = null, string message = null, Func<Exception> exception = null) where T : IComparable<T>
+        {
+            if (parameter.CompareTo(boundary) <= 0)
+                throw exception != null ? exception() : new ArgumentOutOfRangeException(parameterName, parameter, message ?? $"{parameterName ?? "The value"} must be greater than {boundary}, but you specified {parameter}.");
         }
 
         /// <summary>
