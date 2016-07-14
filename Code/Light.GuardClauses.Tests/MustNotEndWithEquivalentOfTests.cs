cacd76c8e11824bc788c06abf74fe83df12585cb
Kenny Pflug <kenny.pflug@live.de>
2016-07-14 11:23:47 +0200

    added MustNotEndWithEquivalentOf

--- Code/Light.GuardClauses.Tests/MustNotEndWithEquivalentOfTests.cs
+++ Code/Light.GuardClauses.Tests/MustNotEndWithEquivalentOfTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using FluentAssertions;
 using Light.GuardClauses.Exceptions;
+using Light.GuardClauses.FrameworkExtensions;
 using Light.GuardClauses.Tests.CustomMessagesAndExceptions;
 using Xunit;
 
@@ -9,11 +12,11 @@ namespace Light.GuardClauses.Tests
     [Trait("Category", Traits.FunctionalTests)]
     public sealed class MustNotEndWithEquivalentOfTests : ICustomMessageAndExceptionTestDataProvider
     {
-        [Theory(DisplayName = "MustNotEndWithEquivalentOf must throw a StringException when the string ends with the specified text (case-sensitivity ignored).")]
+        [Theory(DisplayName = "MustNotEndWithEquivalentOf for strings must throw a StringException when the string ends with the specified text (case-sensitivity ignored).")]
         [InlineData("Foo", "Foo")]
         [InlineData("Bar", "bAR")]
         [InlineData("Death is so terribly final, while life is full of possibilities", "Full of Possibilities")]
-        public void EndsEqual(string @string, string endText)
+        public void StringEndsEqual(string @string, string endText)
         {
             Action act = () => @string.MustNotEndWithEquivalentOf(endText, nameof(@string));
 
@@ -21,31 +24,67 @@ namespace Light.GuardClauses.Tests
                .And.Message.Should().Contain($"{nameof(@string)} must not end with equivalent of \"{endText}\", but you specified {@string}.");
         }
 
-        [Theory(DisplayName = "MustNotEndWithEquivalentOf must not throw an exception when the string does not end with the specified text (case-sensitivity ignored).")]
+        [Theory(DisplayName = "MustNotEndWithEquivalentOf for strings must not throw an exception when the string does not end with the specified text (case-sensitivity ignored).")]
         [InlineData("Foo", "Bar")]
         [InlineData("And I have a tender spot in my heart for cripples and bastards and broken things", "broken glass")]
-        public void EndsDiffer(string @string, string endText)
+        public void StringEndsDiffer(string @string, string endText)
         {
             Action act = () => @string.MustNotEndWithEquivalentOf(endText);
 
             act.ShouldNotThrow();
         }
 
-        [Theory(DisplayName = "MustNotEndWithEquivalentOf must throw an ArgumentNullException when either paramter or text is null.")]
+        [Theory(DisplayName = "MustNotEndWithEquivalentOf for strings must throw an ArgumentNullException when either paramter or text is null.")]
         [InlineData(null, "Foo")]
         [InlineData("Foo", null)]
-        public void ArgumentNull(string @string, string endText)
+        public void StringArgumentNull(string @string, string endText)
         {
             Action act = () => @string.MustNotEndWithEquivalentOf(endText);
 
             act.ShouldThrow<ArgumentNullException>();
         }
 
+        [Theory(DisplayName = "MustNotEndWithEquivalentOf for collections must throw a CollectionException when the collection does end with the specified items in any order.")]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 4, 3, 2 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 3, 2, 4 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 3, 4 })]
+        [InlineData(new[] { -1255 }, new[] { -1255 })]
+        public void CollectionEndsEqual(IEnumerable<int> collection, IEnumerable<int> set)
+        {
+            Action act = () => collection.MustNotEndWithEquivalentOf(set, nameof(collection));
+
+            act.ShouldThrow<CollectionException>()
+               .And.Message.Should().Contain($"{nameof(collection)} must not end with the following items in any order:{new StringBuilder().AppendLine().AppendItemsWithNewLine(set).AppendLine()}but it does.");
+        }
+
+        [Theory(DisplayName = "MustNotEndWithEquivalentOf for collections must not throw an exception when the collection does not end with the specified items in any order.")]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 42, 87 })]
+        [InlineData(new[] { 1, 2, 3, 4 }, new[] { 2, 3 })]
+        [InlineData(new[] { 603 }, new[] { 499, 717 })]
+        public void CollectionEndsDiffer(IEnumerable<int> collection, IEnumerable<int> set)
+        {
+            Action act = () => collection.MustNotEndWithEquivalentOf(set);
+
+            act.ShouldNotThrow();
+        }
+
+        [Theory(DisplayName = "MustNotEndWithEquivalentOf for collections must throw an ArgumentNullException when parameter or set is null.")]
+        [InlineData(new object[] { }, null)]
+        [InlineData(null, new object[] { })]
+        public void CollectionArgumentNull(IEnumerable<object> collection, IEnumerable<object> set)
+        {
+            Action act = () => collection.MustNotEndWithEquivalentOf(set);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
         public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
         {
-            testData.Add(new CustomExceptionTest(exception => "Foo".MustNotEndWithEquivalentOf("foo", exception: exception)));
+            testData.Add(new CustomExceptionTest(exception => "Foo".MustNotEndWithEquivalentOf("foo", exception: exception)))
+                    .Add(new CustomMessageTest<StringException>(message => "Bar".MustNotEndWithEquivalentOf("Bar", message: message)));
 
-            testData.Add(new CustomMessageTest<StringException>(message => "Bar".MustNotEndWithEquivalentOf("Bar", message: message)));
+            testData.Add(new CustomExceptionTest(exception => new[] { 1, 2, 3 }.MustNotEndWithEquivalentOf(new[] { 3, 2 }, exception: exception)))
+                    .Add(new CustomMessageTest<CollectionException>(message => new[] { 1, 2, 3 }.MustNotEndWithEquivalentOf(new[] { 3, 2 }, message: message)));
         }
     }
 }
