eff5e75dd07eeba2c51a4bd3e383e10770d073e4
Kenny Pflug <kenny.pflug@live.de>
2017-01-26 19:32:02 +0100

    added MustBeLocal

---
+++ Code/Light.GuardClauses.Tests/MustBeLocalTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Exceptions;
+using Light.GuardClauses.Tests.CustomMessagesAndExceptions;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace Light.GuardClauses.Tests
+{
+    [Trait("Category", Traits.FunctionalTests)]
+    public sealed class MustBeLocalTests : ICustomMessageAndExceptionTestDataProvider
+    {
+        [Theory(DisplayName = "MustBeLocal must throw an exception when the specified date time's kind is DateTimeKind.Utc or DateTimeKind.Unspecified.")]
+        [MemberData(nameof(NotLocalData))]
+        public void NotLocal(DateTime invalidDateTime)
+        {
+            Action act = () => invalidDateTime.MustBeLocal(nameof(invalidDateTime));
+
+            act.ShouldThrow<InvalidDateTimeException>()
+               .And.Message.Should().Contain($"The specified date time \"{invalidDateTime:O}\" must be of kind local, but actually is {invalidDateTime.Kind}.");
+        }
+
+        public static readonly TestData NotLocalData =
+            new[]
+            {
+                new object[] { DateTime.UtcNow },
+                new object[] { new DateTime(2017, 1, 26, 19, 18, 42, DateTimeKind.Unspecified) },
+                new object[] { new DateTime(2017, 1, 26, 18, 19, 39, DateTimeKind.Utc) }
+            };
+
+        [Theory(DisplayName = "MustBeLocal must not throw an exception when the specified date time's kind is DateTimeKind.Local.")]
+        [MemberData(nameof(LocalData))]
+        public void Local(DateTime dateTime)
+        {
+            Action act = () => dateTime.MustBeLocal();
+
+            act.ShouldNotThrow();
+        }
+
+        public static readonly TestData LocalData =
+            new[]
+            {
+                new object[] { DateTime.Now },
+                new object[] { new DateTime(2017, 1, 26, 19, 22, 26, DateTimeKind.Local) }
+            };
+
+        public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
+        {
+            testData.Add(new CustomExceptionTest(exception => DateTime.UtcNow.MustBeLocal(exception: exception)))
+                    .Add(new CustomMessageTest<InvalidDateTimeException>(message => DateTime.UtcNow.MustBeLocal(message: message)));
+        }
+    }
+}
--- Code/Light.GuardClauses/DateTimeAssertions.cs
+++ Code/Light.GuardClauses/DateTimeAssertions.cs
@@ -23,10 +23,28 @@ namespace Light.GuardClauses
         [Conditional(Check.CompileAssertionsSymbol)]
         public static void MustBeUtc(this DateTime dateTime, string parameterName = null, string message = null, Func<Exception> exception = null)
         {
-            if (dateTime.Kind == DateTimeKind.Utc)
-                return;
+            if (dateTime.Kind == DateTimeKind.Utc) return;
 
             throw exception != null ? exception() : new InvalidDateTimeException(dateTime, message ?? $"The specified date time \"{dateTime:O}\" must be of kind UTC, but actually is {dateTime.Kind}.", parameterName);
+        }
+
+        /// <summary>
+        ///     Checks if the specified value is a local date time, or otherwise throws an <see cref="InvalidDateTimeException" />.
+        /// </summary>
+        /// <param name="dateTime">The value to be checked.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message to be injected into the <see cref="InvalidDateTimeException" /> (optional).</param>
+        /// <param name="exception">
+        ///     The exception that is thrown when <paramref name="dateTime" />'s kind is not <see cref="DateTimeKind.Local" /> (optional).
+        ///     Please note that <paramref name="parameterName" /> and <paramref name="message" /> are both ignored when you specify exception.
+        /// </param>
+        /// <exception cref="InvalidDateTimeException">Thrown when <paramref name="dateTime" />'s is not <see cref="DateTimeKind.Local" />.</exception>
+        [Conditional(Check.CompileAssertionsSymbol)]
+        public static void MustBeLocal(this DateTime dateTime, string parameterName = null, string message = null, Func<Exception> exception = null)
+        {
+            if (dateTime.Kind == DateTimeKind.Local) return;
+
+            throw exception != null ? exception() : new InvalidDateTimeException(dateTime, message ?? $"The specified date time \"{dateTime:O}\" must be of kind local, but actually is {dateTime.Kind}.", parameterName);
         }
     }
 }
