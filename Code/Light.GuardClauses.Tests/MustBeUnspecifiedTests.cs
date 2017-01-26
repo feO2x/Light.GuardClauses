6e0e08f197da0dca976c33fc05ed0b895f9aefb8
Kenny Pflug <kenny.pflug@live.de>
2017-01-26 19:46:01 +0100

    added MustBeUnspecified

---
+++ Code/Light.GuardClauses.Tests/MustBeUnspecifiedTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Exceptions;
+using Light.GuardClauses.Tests.CustomMessagesAndExceptions;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace Light.GuardClauses.Tests
+{
+    [Trait("Category", Traits.FunctionalTests)]
+    public sealed class MustBeUnspecifiedTests : ICustomMessageAndExceptionTestDataProvider
+    {
+        [Theory(DisplayName = "MustBeUnspecified must throw an exception when the specified date time's kind is DateTimeKind.Utc or DateTimeKind.Local.")]
+        [MemberData(nameof(NotUnspecifiedData))]
+        public void NotUnspecified(DateTime invalidDateTime)
+        {
+            Action act = () => invalidDateTime.MustBeUnspecified(nameof(invalidDateTime));
+
+            act.ShouldThrow<InvalidDateTimeException>()
+               .And.Message.Should().Contain($"The specified date time \"{invalidDateTime:O}\" must be of kind unspecified, but actually is {invalidDateTime.Kind}.");
+        }
+
+        public static readonly TestData NotUnspecifiedData =
+            new[]
+            {
+                new object[] { DateTime.UtcNow },
+                new object[] { new DateTime(2017, 1, 26, 19, 18, 42, DateTimeKind.Local) },
+                new object[] { new DateTime(2017, 1, 26, 18, 19, 39, DateTimeKind.Utc) }
+            };
+
+        [Theory(DisplayName = "MustBeUnspecified must not throw an exception when the specified date time's kind is DateTimeKind.Unspecified.")]
+        [MemberData(nameof(UnspecifiedData))]
+        public void Unspecified(DateTime dateTime)
+        {
+            Action act = () => dateTime.MustBeUnspecified();
+
+            act.ShouldNotThrow();
+        }
+
+        public static readonly TestData UnspecifiedData =
+            new[]
+            {
+                new object[] { DateTime.MinValue },
+                new object[] { new DateTime(2017, 1, 26, 19, 22, 26, DateTimeKind.Unspecified) }
+            };
+
+        public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
+        {
+            testData.Add(new CustomExceptionTest(exception => DateTime.UtcNow.MustBeUnspecified(exception: exception)))
+                    .Add(new CustomMessageTest<InvalidDateTimeException>(message => DateTime.UtcNow.MustBeUnspecified(message: message)));
+        }
+    }
+}
--- Code/Light.GuardClauses/DateTimeAssertions.cs
+++ Code/Light.GuardClauses/DateTimeAssertions.cs
@@ -46,5 +46,24 @@ namespace Light.GuardClauses
 
             throw exception != null ? exception() : new InvalidDateTimeException(dateTime, message ?? $"The specified date time \"{dateTime:O}\" must be of kind local, but actually is {dateTime.Kind}.", parameterName);
         }
+
+        /// <summary>
+        ///     Checks if the specified value is date time with <see cref="DateTimeKind.Unspecified" />, or otherwise throws an <see cref="InvalidDateTimeException" />.
+        /// </summary>
+        /// <param name="dateTime">The value to be checked.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message to be injected into the <see cref="InvalidDateTimeException" /> (optional).</param>
+        /// <param name="exception">
+        ///     The exception that is thrown when <paramref name="dateTime" />'s kind is not <see cref="DateTimeKind.Unspecified" /> (optional).
+        ///     Please note that <paramref name="parameterName" /> and <paramref name="message" /> are both ignored when you specify exception.
+        /// </param>
+        /// <exception cref="InvalidDateTimeException">Thrown when <paramref name="dateTime" />'s is not <see cref="DateTimeKind.Unspecified" />.</exception>
+        [Conditional(Check.CompileAssertionsSymbol)]
+        public static void MustBeUnspecified(this DateTime dateTime, string parameterName = null, string message = null, Func<Exception> exception = null)
+        {
+            if (dateTime.Kind == DateTimeKind.Unspecified) return;
+
+            throw exception != null ? exception() : new InvalidDateTimeException(dateTime, message ?? $"The specified date time \"{dateTime:O}\" must be of kind unspecified, but actually is {dateTime.Kind}.", parameterName);
+        }
     }
 }
