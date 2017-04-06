e5320b3e509580b1d6938256096ab828dec72090
Kenny Pflug <kenny.pflug@live.de>
2017-04-06 10:04:46 +0200

    added MustBeAbsoluteUri

---
+++ Code/Light.GuardClauses.Tests/MustBeAbsoluteUriTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Tests.CustomMessagesAndExceptions;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace Light.GuardClauses.Tests
+{
+    [Trait("Category", Traits.FunctionalTests)]
+    public sealed class MustBeAbsoluteUriTests : ICustomMessageAndExceptionTestDataProvider
+    {
+        [Theory(DisplayName = "MustBeAbsoluteUri must not throw an exception when the specified uri is an absolute one.")]
+        [MemberData(nameof(AbsoluteUriData))]
+        public void AbsoluteUri(Uri absoluteUri)
+        {
+            Action act = () => absoluteUri.MustBeAbsoluteUri();
+
+            act.ShouldNotThrow();
+        }
+
+        public static readonly TestData AbsoluteUriData =
+            new[]
+            {
+                new object[] { new Uri("http://localhost:8080/api/contacts/") },
+                new object[] { new Uri("https://my.service.com/contacts/new") }
+            };
+
+        [Theory(DisplayName = "MustBeAbsoluteUri must throw an ArgumentException when the specified uri is a relative one.")]
+        [MemberData(nameof(RelativeUriData))]
+        public void RelativeUri(Uri relativeUri)
+        {
+            Action act = () => relativeUri.MustBeAbsoluteUri(nameof(relativeUri));
+
+            act.ShouldThrow<ArgumentException>()
+               .And.Message.Should().Contain($"{nameof(relativeUri)} must be an absolute URI, but you specified \"{relativeUri}\".");
+        }
+
+        public static readonly TestData RelativeUriData =
+            new[]
+            {
+                new object[] { new Uri("/api/orders", UriKind.Relative) },
+                new object[] { new Uri("/contacts/new", UriKind.Relative) }
+            };
+
+        [Fact]
+        public void UriNull()
+        {
+            Action act = () => ((Uri) null).MustBeAbsoluteUri();
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
+        {
+            testData.Add(new CustomExceptionTest(exception => new Uri("/api/buildings", UriKind.Relative).MustBeAbsoluteUri(exception: exception)))
+                    .Add(new CustomMessageTest<ArgumentException>(message => new Uri("/api/buildings", UriKind.Relative).MustBeAbsoluteUri(message: message)));
+        }
+    }
+}
---
+++ Code/Light.GuardClauses/UriExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Light.GuardClauses
+{
+    /// <summary>
+    ///     Provides assertion methods for the <see cref="Uri" /> class.
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        ///     Ensures that the specified URI is an absolute one, or else throws an <see cref="ArgumentException" />.
+        /// </summary>
+        /// <param name="uri">The URI to be checked.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message of the exception (optional).</param>
+        /// <param name="exception">
+        ///     The exception that will be thrown when <paramref name="uri" /> is not an absolute URI.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
+        /// </param>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="uri" /> is not an absolute URI.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="uri" /> is null.</exception>
+        [Conditional(Check.CompileAssertionsSymbol)]
+        public static void MustBeAbsoluteUri(this Uri uri, string parameterName = null, string message = null, Func<Exception> exception = null)
+        {
+            uri.MustNotBeNull(parameterName);
+
+            if (uri.IsAbsoluteUri)
+                return;
+
+            throw exception != null ? exception() : new ArgumentException(message ?? $"{parameterName ?? "The uri"} must be an absolute URI, but you specified \"{uri}\".");
+        }
+    }
+}
