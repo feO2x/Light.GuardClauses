819978f946b4e27606965f4ca2507f679041a3ad
Kenny Pflug <kenny.pflug@live.de>
2017-04-06 19:06:21 +0200

    added MustHaveScheme

---
+++ Code/Light.GuardClauses.Tests/MustHaveSchemeTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Tests.CustomMessagesAndExceptions;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace Light.GuardClauses.Tests
+{
+    [Trait("Category", Traits.FunctionalTests)]
+    public sealed class MustHaveSchemeTests : ICustomMessageAndExceptionTestDataProvider
+    {
+        [Theory(DisplayName = "MustHaveScheme must throw an ArgumentException when the URI does not have the specified scheme.")]
+        [MemberData(nameof(InvalidSchemeData))]
+        public void InvalidScheme(Uri uri, string scheme)
+        {
+            Action act = () => uri.MustHaveScheme(scheme, nameof(uri));
+
+            act.ShouldThrow<ArgumentException>()
+               .And.Message.Should().Contain($"{nameof(uri)} must have scheme \"{scheme}\"");
+        }
+
+        public static readonly TestData InvalidSchemeData =
+            new[]
+            {
+                new object[] { new Uri("http://localhost:8080"), "https" },
+                new object[] { new Uri("http://my.service.com/upload"), "ftp" },
+                new object[] { new Uri("/api/contacts", UriKind.Relative), "https" }
+            };
+
+        [Theory]
+        [MemberData(nameof(ValidSchemeData))]
+        public void ValidScheme(Uri uri, string scheme)
+        {
+            Action act = () => uri.MustHaveScheme(scheme);
+
+            act.ShouldNotThrow();
+        }
+
+        public static readonly TestData ValidSchemeData =
+            new[]
+            {
+                new object[] { new Uri("https://www.google.com"), "https" },
+                new object[] { new Uri("ftps://192.168.177.2"), "ftps" }
+            };
+
+        [Fact]
+        public void UriNull()
+        {
+            Action act = () => ((Uri) null).MustHaveScheme("foo");
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
+        public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
+        {
+            testData.Add(new CustomExceptionTest(exception => new Uri("http://localhost").MustHaveScheme("https", exception: exception)))
+                    .Add(new CustomMessageTest<ArgumentException>(message => new Uri("http://localhost").MustHaveScheme("https", message: message)));
+        }
+    }
+}
--- Code/Light.GuardClauses/UriExtensions.cs
+++ Code/Light.GuardClauses/UriExtensions.cs
@@ -9,7 +9,7 @@ namespace Light.GuardClauses
     public static class UriExtensions
     {
         /// <summary>
-        ///     Ensures that the specified URI is an absolute one, or else throws an <see cref="ArgumentException" />.
+        ///     Ensures that the specified URI is an absolute one, or otherwise throws an <see cref="ArgumentException" />.
         /// </summary>
         /// <param name="uri">The URI to be checked.</param>
         /// <param name="parameterName">The name of the parameter (optional).</param>
@@ -28,7 +28,32 @@ namespace Light.GuardClauses
             if (uri.IsAbsoluteUri)
                 return;
 
-            throw exception != null ? exception() : new ArgumentException(message ?? $"{parameterName ?? "The uri"} must be an absolute URI, but you specified \"{uri}\".");
+            throw exception != null ? exception() : new ArgumentException(message ?? $"{parameterName ?? "The URI"} must be an absolute URI, but you specified \"{uri}\".", parameterName);
+        }
+
+        /// <summary>
+        ///     Ensures that the specified URI has the given scheme, or otherwise throws an <see cref="ArgumentException" />.
+        /// </summary>
+        /// <param name="uri">The URI to be checked.</param>
+        /// <param name="scheme">The scheme that the URI should have.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message of the exception (optional).</param>
+        /// <param name="exception">
+        ///     The exception that will be thrown when <paramref name="uri" /> is not an absolute URI.
+        ///     Please note that <paramref name="message" /> and <paramref name="parameterName" /> are both ignored when you specify exception.
+        /// </param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="uri" /> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="uri" /> does not have the specified scheme.</exception>
+        [Conditional(Check.CompileAssertionsSymbol)]
+        public static void MustHaveScheme(this Uri uri, string scheme, string parameterName = null, string message = null, Func<Exception> exception = null)
+        {
+            uri.MustNotBeNull(parameterName);
+
+            if (uri.IsAbsoluteUri && uri.Scheme == scheme)
+                return;
+
+            var subclause = uri.IsAbsoluteUri ? $"but actually has scheme \"{uri.Scheme}\" (\"{uri}\")." : $"but it has none because it is a relative URI (\"{uri}\").";
+            throw exception != null ? exception() : new ArgumentException(message ?? $"{parameterName ?? "The URI"} must have scheme \"{scheme}\", {subclause}");
         }
     }
 }
