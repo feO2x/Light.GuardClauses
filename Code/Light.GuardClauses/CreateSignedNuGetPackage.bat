1a7f09a82b7d2e6604f66d1d6aadc04c90781b51
Kenny Pflug <kenny.pflug@live.de>
2017-05-28 19:54:33 +0200

    CreateSignedNuGetPackage.bat now uses msbuild 15

--- Code/Light.GuardClauses/CreateSignedNuGetPackage.bat
+++ Code/Light.GuardClauses/CreateSignedNuGetPackage.bat
@@ -1,10 +1,1 @@
-copy bin\Release\Light.GuardClauses.dll bin\signedRelease\ /Y
-copy bin\Release\Light.GuardClauses.xml bin\signedRelease\ /Y
-
-ildasm bin\signedRelease\Light.GuardClauses.dll /out:bin\signedRelease\Light.GuardClauses.il
-del bin\signedRelease\Light.GuardClauses.dll
-ilasm bin\signedRelease\Light.GuardClauses.il /dll /key=bin\signedRelease\Light.GuardClauses.snk
-del bin\signedRelease\Light.GuardClauses.il
-del bin\signedRelease\Light.GuardClauses.res
-
-nuget pack Light.GuardClauses.nuspec
+msbuild Light.GuardClauses.csproj /t:rebuild /t:pack /p:Configuration=Release /p:SignAssembly=true /p:AssemblyOriginatorKeyFile=bin\Light.GuardClauses.snk
