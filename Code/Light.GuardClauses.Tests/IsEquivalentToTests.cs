f85a624dd8c9b8d7b339a96a5d4715325511ce89
Kenny Pflug <kenny.pflug@live.de>
2017-06-17 08:38:05 +0200

    added further tests for IsEquivalentTo

--- Code/Light.GuardClauses.Tests/IsEquivalentToTests.cs
+++ Code/Light.GuardClauses.Tests/IsEquivalentToTests.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-using FluentAssertions;
 using Xunit;
 using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
 // ReSharper disable UnusedTypeParameter
 
 namespace Light.GuardClauses.Tests
@@ -27,8 +29,6 @@ namespace Light.GuardClauses.Tests
         }
 
         [Theory(DisplayName = "IsEquivalentTo must return true when one type is a constructed generic type and the other one is the corresponding generic type definition.")]
-        [InlineData(typeof(List<>), typeof(List<>), true)]
-        [InlineData(typeof(IList<string>), typeof(IList<string>), true)]
         [MemberData(nameof(GenericTypesData))]
         public void GenericTypes(Type type, Type other, bool expected)
         {
@@ -37,15 +37,42 @@ namespace Light.GuardClauses.Tests
             result.Should().Be(expected);
         }
 
-        public static readonly TestData GenericTypesData =
+        public static TestData GenericTypesData =>
             new[]
             {
+                // Simple type tests
+                new object[] { typeof(List<>), typeof(List<>), true },
+                new object[] { typeof(IList<string>), typeof(IList<string>), true },
+
+                // Base types of generic types
                 new object[] { typeof(SubTypeA<>).GetTypeInfo().BaseType, typeof(GenericType<>), true },
+                new object[] { typeof(GenericType<>), typeof(SubTypeA<>).GetTypeInfo().BaseType, true },
+                new object[] { typeof(SubTypeA<int>).GetTypeInfo().BaseType, typeof(GenericType<int>), true },
                 new object[] { typeof(SubTypeA<int>).GetTypeInfo().BaseType, typeof(GenericType<int>), true },
                 new object[] { typeof(SubTypeA<int>).GetTypeInfo().BaseType, typeof(GenericType<>), true },
                 new object[] { typeof(SubTypeB).GetTypeInfo().BaseType, typeof(GenericType<string>), true },
                 new object[] { typeof(SubTypeB).GetTypeInfo().BaseType, typeof(GenericType<>), true },
-                new object[] { typeof(SubTypeB).GetTypeInfo().BaseType, typeof(List<>), false}
+                new object[] { typeof(SubTypeB).GetTypeInfo().BaseType, typeof(List<>), false },
+
+                // Same list as above, but switched first and second parameter
+                new object[] { typeof(GenericType<>), typeof(SubTypeA<>).GetTypeInfo().BaseType, true },
+                new object[] { typeof(GenericType<>), typeof(SubTypeA<>).GetTypeInfo().BaseType, true },
+                new object[] { typeof(GenericType<int>), typeof(SubTypeA<int>).GetTypeInfo().BaseType, true },
+                new object[] { typeof(GenericType<int>), typeof(SubTypeA<int>).GetTypeInfo().BaseType, true },
+                new object[] { typeof(GenericType<>), typeof(SubTypeA<int>).GetTypeInfo().BaseType, true },
+                new object[] { typeof(GenericType<string>), typeof(SubTypeB).GetTypeInfo().BaseType, true },
+                new object[] { typeof(GenericType<>), typeof(SubTypeB).GetTypeInfo().BaseType, true },
+                new object[] { typeof(List<>), typeof(SubTypeB).GetTypeInfo().BaseType, false },
+
+                // Return types of generic methods
+                new object[] { CreateDictionaryOfTKeyTValue.ReturnType, typeof(Dictionary<,>), true },
+                new object[] { CreateDictionaryOfTKey.ReturnType, typeof(Dictionary<,>), true },
+                new object[] { CreateDictionaryOfTKey.ReturnType, CreateDictionaryOfTKeyTValue.ReturnType, false },
+
+                // Same list as above, but switched first and second parameter
+                new object[] { typeof(Dictionary<,>), CreateDictionaryOfTKeyTValue.ReturnType, true },
+                new object[] { typeof(Dictionary<,>), CreateDictionaryOfTKey.ReturnType, true },
+                new object[] { CreateDictionaryOfTKeyTValue.ReturnType, CreateDictionaryOfTKey.ReturnType, false }
             };
 
         public class GenericType<T> { }
@@ -53,5 +80,27 @@ namespace Light.GuardClauses.Tests
         public class SubTypeA<T> : GenericType<T> { }
 
         public class SubTypeB : GenericType<string> { }
+
+        public static Dictionary<TKey, TValue> CreateDictionaryA<TKey, TValue>()
+        {
+            return new Dictionary<TKey, TValue>();
+        }
+
+        public static Dictionary<TKey, object> CreateDictionaryB<TKey>()
+        {
+            return new Dictionary<TKey, object>();
+        }
+
+        private static readonly MethodInfo CreateDictionaryOfTKeyTValue;
+        private static readonly MethodInfo CreateDictionaryOfTKey;
+
+        static IsEquivalentToTests()
+        {
+            var typeInfo = typeof(IsEquivalentToTests).GetTypeInfo();
+            CreateDictionaryOfTKeyTValue = typeInfo.DeclaredMethods.First(m => m.IsStatic && m.Name.StartsWith(nameof(CreateDictionaryA)));
+            CreateDictionaryOfTKey = typeInfo.DeclaredMethods.First(m => m.IsStatic && m.Name.StartsWith(nameof(CreateDictionaryB)));
+        }
     }
+
+    
 }
