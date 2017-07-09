1934cc5c55eabb5d50250c91800231475e6074df
Kenny Pflug <kenny.pflug@live.de>
2017-07-09 20:30:25 +0200

    cleaned Trait category in IsClassTests and IsDelegateTests

--- Code/Light.GuardClauses.Tests/IsClassTests.cs
+++ Code/Light.GuardClauses.Tests/IsClassTests.cs
@@ -6,7 +6,7 @@ using Xunit;
 
 namespace Light.GuardClauses.Tests
 {
-    [Trait("Category", "Functional Tests")]
+    [Trait("Category", Traits.FunctionalTests)]
     public sealed class IsClassTests
     {
         [Fact]
