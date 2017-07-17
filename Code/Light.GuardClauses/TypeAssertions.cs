699567d68c0cf15b46219b6799df5ca7ccc4d81d
Kenny Pflug <kenny.pflug@live.de>
2017-07-17 19:44:25 +0200

    simplified XML comment for IsEquivalentTo

--- Code/Light.GuardClauses/TypeAssertions.cs
+++ Code/Light.GuardClauses/TypeAssertions.cs
@@ -11,13 +11,13 @@ namespace Light.GuardClauses
     public static class TypeAssertions
     {
         /// <summary>
-        ///     Checks if the two specified types are equivalent. This is true when 1) both types are non-generic types and are equal
-        ///     or when 2) one type is a constructed generic type and the other type is the corresponding generic type definition.
+        ///     Checks if the two specified types are equivalent. This is true when 1) both types are equal or 
+        ///     when 2) one type is a constructed generic type and the other type is the corresponding generic type definition.
         /// </summary>
         /// <param name="type">The first type to be checked.</param>
         /// <param name="other">The other type to be checked.</param>
         /// <returns>
-        ///     True if both types are null, or if both are non-generic types that are equal, or if one type
+        ///     True if both types are null, or if both are equal, or if one type
         ///     is a constructed generic type and the other one is the corresponding generic type definition, else false.
         /// </returns>
         public static bool IsEquivalentTo(this Type type, Type other)
