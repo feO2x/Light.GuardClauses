cdb34f1963f4dd8a276ba024bea98636697c4d0b
Kenny Pflug <kenny.pflug@live.de>
2017-07-18 07:24:23 +0200

    moved tests of SingleItemMustContainTests and SingleItemMustNotContainTests to MustContainTests and MustNotContainTests, respectively

--- Code/Light.GuardClauses.Tests/EnumerableAssertionsTests/MustNotContainTests.cs
+++ Code/Light.GuardClauses.Tests/EnumerableAssertionsTests/MustNotContainTests.cs
@@ -12,6 +12,52 @@ namespace Light.GuardClauses.Tests.EnumerableAssertionsTests
     [Trait("Category", Traits.FunctionalTests)]
     public sealed class MustNotContainTests : ICustomMessageAndExceptionTestDataProvider
     {
+        [Theory(DisplayName = "MustNotContain must throw a CollectionException when the specified item is part of the collection.")]
+        [MemberData(nameof(CollectionContainsItemData))]
+        public void CollectionContainsItem<T>(T[] collection, T item)
+        {
+            Action act = () => collection.MustNotContain(item, nameof(collection));
+
+            act.ShouldThrow<CollectionException>()
+               .And.Message.Should().Contain($"{nameof(collection)} must not contain value \"{item.ToStringOrNull()}\", but it does.");
+        }
+
+        public static readonly TestData CollectionContainsItemData =
+            new[]
+            {
+                new object[] { new[] { 1, 2, 3 }, 2 },
+                new object[] { new[] { "Hello", "World" }, "Hello" },
+                new object[] { new[] { "There is something", null }, null }
+            };
+
+        [Theory(DisplayName = "MustNotContain must not throw an exception when the specified item is not part of the collection.")]
+        [MemberData(nameof(CollectionDoesNotContainItemData))]
+        public void CollectionDoesNotContainItem<T>(T[] collection, T item)
+        {
+            Action act = () => collection.MustNotContain(item);
+
+            act.ShouldNotThrow();
+        }
+
+        public static readonly TestData CollectionDoesNotContainItemData =
+            new[]
+            {
+                new object[] { new[] { 'a', 'b', 'c' }, 'd' },
+                new object[] { new[] { 42, 87, -188 }, 10555 },
+                new object[] { new[] { true }, false }
+            };
+
+        [Fact(DisplayName = "MustNotContain must throw an ArgumentNullException when the specified collection is null.")]
+        public void CollectionNull()
+        {
+            object[] collection = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action act = () => collection.MustNotContain("foo");
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+
         [Theory(DisplayName = "MustNotContain must throw a CollectionException when the collection contains any of the items of the specified set.")]
         [MemberData(nameof(SetContainedData))]
         public void SetContained<T>(T[] collection, T[] set)
@@ -58,9 +104,11 @@ namespace Light.GuardClauses.Tests.EnumerableAssertionsTests
 
         public void PopulateTestDataForCustomExceptionAndCustomMessageTests(CustomMessageAndExceptionTestData testData)
         {
-            testData.Add(new CustomExceptionTest(exception => new[] { 'a', 'b' }.MustNotContain(new[] { 'a' }, exception: exception)));
+            testData.Add(new CustomExceptionTest(exception => new[] { 1, 2, 3 }.MustNotContain(2, exception: exception)))
+                    .Add(new CustomMessageTest<CollectionException>(message => new[] { 1, 2, 3 }.MustNotContain(2, message: message)));
 
-            testData.Add(new CustomMessageTest<CollectionException>(message => new[] { 'a', 'b' }.MustNotContain(new[] { 'a' }, message: message)));
+            testData.Add(new CustomExceptionTest(exception => new[] { 'a', 'b' }.MustNotContain(new[] { 'a' }, exception: exception)))
+                    .Add(new CustomMessageTest<CollectionException>(message => new[] { 'a', 'b' }.MustNotContain(new[] { 'a' }, message: message)));
         }
     }
 }
