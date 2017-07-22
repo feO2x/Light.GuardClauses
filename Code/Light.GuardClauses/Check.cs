b3f217b2c9b2bff3da429c8747c249e654c669db
Kenny Pflug <kenny.pflug@live.de>
2017-07-22 17:28:26 +0200

    The createException parameters in class Check are now names "exception", as it is with all other assertion methods

--- Code/Light.GuardClauses/Check.cs
+++ Code/Light.GuardClauses/Check.cs
@@ -4,7 +4,7 @@ namespace Light.GuardClauses
 {
     /// <summary>
     ///     The <see cref="Check" /> class is the entry point to using assertions in production code: it defines the two
-    ///     static methods <see cref="That" /> and <see cref="Against" /> that form the most general entry point to the library.
+    ///     static methods <see cref="That" /> and <see cref="Against" /> that form the most general way to express assertions.
     /// </summary>
     public static class Check
     {
@@ -12,28 +12,28 @@ namespace Light.GuardClauses
         ///     Ensures that the specified <paramref name="assertionResult" /> is true, or otherwise throws the specified exception.
         /// </summary>
         /// <param name="assertionResult">The result of an assertion to be checked.</param>
-        /// <param name="createException">The delegate that creates the exception to be thrown when the <paramref name="assertionResult" /> is false.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="createException" /> is null.</exception>
-        public static void That(bool assertionResult, Func<Exception> createException)
+        /// <param name="exception">The delegate that creates the exception to be thrown when the <paramref name="assertionResult" /> is false.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception" /> is null.</exception>
+        public static void That(bool assertionResult, Func<Exception> exception)
         {
-            createException.MustNotBeNull(nameof(createException));
+            exception.MustNotBeNull(nameof(exception));
 
             if (assertionResult == false)
-                throw createException();
+                throw exception();
         }
 
         /// <summary>
         ///     Ensures that the specified <paramref name="assertionResult" /> is false, or otherwise throws the specified exception.
         /// </summary>
         /// <param name="assertionResult">The result of an assertion to be checked.</param>
-        /// <param name="createException">The delegate that creates the exception to be thrown when the <paramref name="assertionResult" /> is true.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="createException" /> is null.</exception>
-        public static void Against(bool assertionResult, Func<Exception> createException)
+        /// <param name="exception">The delegate that creates the exception to be thrown when the <paramref name="assertionResult" /> is true.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="exception" /> is null.</exception>
+        public static void Against(bool assertionResult, Func<Exception> exception)
         {
-            createException.MustNotBeNull(nameof(createException));
+            exception.MustNotBeNull(nameof(exception));
 
             if (assertionResult)
-                throw createException();
+                throw exception();
         }
     }
 }
