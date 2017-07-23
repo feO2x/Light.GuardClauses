06cc0d82de66abb41b380da62bc468b3449b8d8b
Kenny Pflug <kenny.pflug@live.de>
2017-07-23 19:56:17 +0200

    updated csproj for v3.0.0, added missing XML comments

--- Code/Light.GuardClauses/IgnoreCaseInfo.cs
+++ Code/Light.GuardClauses/IgnoreCaseInfo.cs
@@ -32,18 +32,26 @@ namespace Light.GuardClauses
             CompareOptions = compareOptions.MustBeValidEnumValue(nameof(compareOptions));
         }
 
+        /// <summary>
+        ///     Creates an <see cref="IgnoreCaseInfo" /> with the invariant culture and CompareOptions.OrdinalIgnoreCase when the
+        ///     specified boolean is true.
+        /// </summary>
         public static implicit operator IgnoreCaseInfo(bool ignoreCase)
         {
             return ignoreCase ? new IgnoreCaseInfo(CultureInfo.InvariantCulture, CompareOptions.OrdinalIgnoreCase) : new IgnoreCaseInfo();
         }
 
+        /// <summary>
+        ///     Creates an <see cref="IgnoreCaseInfo" /> with the specified culture info and CompareOptions.OrdinalIgnoreCase.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="cultureInfo" /> is null.</exception>
         public static implicit operator IgnoreCaseInfo(CultureInfo cultureInfo)
         {
             return new IgnoreCaseInfo(cultureInfo, CompareOptions.OrdinalIgnoreCase);
         }
 
         /// <summary>
-        ///     Checks if the <paramref name="string"/> contains the <paramref name="substring"/>.
+        ///     Checks if the <paramref name="string" /> contains the <paramref name="substring" />.
         /// </summary>
         /// <param name="string">The string to be checked.</param>
         /// <param name="substring">The string to seek.</param>
