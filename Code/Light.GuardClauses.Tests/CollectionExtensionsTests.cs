128b7228dd860e875be196693c6bb83a3d282b59
Kenny Pflug <kenny.pflug@live.de>
2017-09-02 14:45:27 +0200

    added IndexOf for IReadOnlyList<T>

---
+++ Code/Light.GuardClauses.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Light.GuardClauses.FrameworkExtensions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests
+{
+    [Trait("Category", Traits.FunctionalTests)]
+    public sealed class CollectionExtensionsTests
+    {
+        [Theory(DisplayName = "IndexOf must return the index of the item when the target collection contains it.")]
+        [InlineData(new []{1, 2, 3}, 2, 1)]
+        [InlineData(new []{1, 2, 3, 4, 5}, 5, 4)]
+        [InlineData(new []{42}, 42, 0)]
+        [InlineData(new []{42}, 47, -1)]
+        [InlineData(new string[]{}, "Foo", -1)]
+        [InlineData(new []{"Foo", "Bar", "Baz"}, "Qux", -1)]
+        public void IndexOf<T>(IReadOnlyList<T> collection, T item, int expectedIndex)
+        {
+            var index = collection.IndexOf(item);
+
+            index.Should().Be(expectedIndex);
+        }
+    }
+}
---
+++ Code/Light.GuardClauses/FrameworkExtensions/CollectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Light.GuardClauses.FrameworkExtensions
+{
+    /// <summary>
+    ///     Provides extension methods for <see cref="IReadOnlyList{T}" />.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        ///     Gets the index of the specified item, or -1 if the it cannot be found.
+        /// </summary>
+        /// <typeparam name="T">The item type of the collection.</typeparam>
+        /// <param name="collection">The collection that might contain the item.</param>
+        /// <param name="item">The item whose index is searched for.</param>
+        /// <param name="equalityComparer">
+        ///     The equality comparer used to compare the items (optional). By default,
+        ///     <see cref="EqualityComparer{T}.Default" /> is used.
+        /// </param>
+        /// <returns>The index of the specified item, or -1 when the item could not be found.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <see cref="collection" /> is null.</exception>
+        public static int IndexOf<T>(this IReadOnlyList<T> collection, T item, IEqualityComparer<T> equalityComparer = null)
+        {
+            collection.MustNotBeNull(nameof(collection));
+            equalityComparer = equalityComparer ?? EqualityComparer<T>.Default;
+
+            for (var i = 0; i < collection.Count; i++)
+            {
+                if (equalityComparer.EqualsWithHashCode(item, collection[i]))
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
