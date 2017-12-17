a8a250479a559d1ea95fe42c0f8669e6e327f6f8
Kenny Pflug <kenny.pflug@live.de>
2017-12-17 13:50:15 +0100

    added Light.GuardClauses.Performance test suite

---
+++ Code/Light.GuardClauses.Performance/NullCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance
+{
+    public class NullCheck
+    {
+        public object Instance = new SampleEntity(Guid.NewGuid());
+
+        [Benchmark(Baseline = true)]
+        public object InlineVersion()
+        {
+            if (Instance == null)
+                throw new ArgumentNullException(nameof(Instance));
+            return Instance;
+        }
+
+        [Benchmark]
+        public object LightGuardClausesVersion()
+        {
+            return Instance.MustNotBeNull(nameof(Instance));
+        }
+
+        [Benchmark]
+        public object NonGenericVersion()
+        {
+            return MustNotBeNull(Instance);
+        }
+
+        private static object MustNotBeNull(object @object, string parameterName = null, string message = null, Func<Exception> exception = null)
+        {
+            if (@object == null)
+                throw exception?.Invoke() ?? new ArgumentNullException(parameterName, message);
+
+            return @object;
+        }
+    }
+
+    public class SampleEntity
+    {
+        public readonly Guid Id;
+
+        public SampleEntity(Guid id)
+        {
+            Id = id.MustNotBeEmpty(nameof(id));
+        }
+    }
+}
---
+++ Code/Light.GuardClauses.Performance/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Light.GuardClauses.Performance
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<NullCheck>();
+        }
+    }
+}
