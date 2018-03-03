30e63e6f98e133ce1e1b3e6ba60a94b62f7ae514
Kenny Pflug <kenny.pflug@live.de>
2018-03-03 07:39:39 +0100

    added benchmarks for Check.InvalidState and Check.InvalidOperation

---
+++ Code/Light.GuardClauses.Performance/CheckAssertions/InvalidOperationBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.CheckAssertions
+{
+    public class InvalidOperationBenchmarks : DefaultBenchmark
+    {
+        private const string Message = "You must not call this method when Condition is true";
+        public bool Condition = false;
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeVersion()
+        {
+            if (Condition) throw new InvalidOperationException(Message);
+            return Condition;
+        }
+
+        [Benchmark]
+        public bool LightGuardClauses()
+        {
+            Check.InvalidOperation(Condition, Message);
+            return Condition;
+        }
+    }
+}
---
+++ Code/Light.GuardClauses.Performance/CheckAssertions/InvalidStateBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using Light.GuardClauses.Exceptions;
+
+namespace Light.GuardClauses.Performance.CheckAssertions
+{
+    public class InvalidStateBenchmarks : DefaultBenchmark
+    {
+        private const string Message = "Condition must not be true.";
+        public bool Condition = false;
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeVersion()
+        {
+            if (Condition) throw new InvalidStateException(Message);
+            return Condition;
+        }
+
+        [Benchmark]
+        public bool LightGuardClauses()
+        {
+            Check.InvalidState(Condition, Message);
+            return Condition;
+        }
+    }
+}
