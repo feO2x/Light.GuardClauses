2442917c055448cd918e58e8842de0c7d17d1564
Kenny Pflug <kenny.pflug@live.de>
2018-03-09 04:56:34 +0100

    added comment for CommonAssertions class

---
+++ Code/Light.GuardClauses/CommonAssertions.cs
@@ -0,0 +1,8 @@
+﻿namespace Light.GuardClauses
+{
+    /// <summary>
+    /// The <see cref="CommonAssertions" /> class contains the most common assertions like MustNotBeNull and assertions that are not directly related to
+    /// any categories like collection assertions or string assertions.
+    /// </summary>
+    public static partial class CommonAssertions { }
+}
