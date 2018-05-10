f95b46ff01de8875d73ec0d407ce93ea5615c7e4
Kenny Pflug <kenny.pflug@live.de>
2018-05-10 07:04:52 +0200

    added MustNotBeGreaterThanOrEqualTo

---
+++ Code/Light.GuardClauses.Performance/ComparableAssertions/MustNotBeGreaterThanOrEqualToBenchmark.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.ComparableAssertions
+{
+    public class MustNotBeGreaterThanOrEqualToBenchmark
+    {
+        public double First = 42.7;
+        public double Second = 78.21;
+
+        [Benchmark(Baseline = true)]
+        public double BaseVersion()
+        {
+            if (First >= Second) throw new ArgumentOutOfRangeException(nameof(First));
+            return First;
+        }
+
+        [Benchmark]
+        public double BaseVersionWithCompareTo()
+        {
+            // ReSharper disable once ImpureMethodCallOnReadonlyValueField
+            if (First.CompareTo(Second) >= 0) throw new ArgumentOutOfRangeException(nameof(First));
+            return First;
+        }
+
+        [Benchmark]
+        public double LightGuardClausesWithParameterName() => First.MustNotBeGreaterThanOrEqualTo(Second, nameof(First));
+
+        [Benchmark]
+        public double LightGuardClausesCustomException() => First.MustNotBeGreaterThanOrEqualTo(Second, (p, b) => new Exception($"{p} is not less than {b}."));
+
+        [Benchmark]
+        public double OldVersion() => First.OldMustNotBeGreaterThanOrEqualTo(Second, nameof(First));
+    }
+
+    public static class MustNotBeGreaterThanOrEqualToExtensionMethods
+    {
+        public static T OldMustNotBeGreaterThanOrEqualTo<T>(this T parameter, T boundary, string parameterName = null, string message = null, Func<Exception> exception = null) where T : IComparable<T>
+        {
+            if (parameter.CompareTo(boundary) < 0)
+                return parameter;
+
+            throw exception != null ? exception() : new ArgumentOutOfRangeException(parameterName, parameter, message ?? $"{parameterName ?? "The value"} must be less than {boundary}, but you specified {parameter}.");
+        }
+    }
+}
--- Code/Light.GuardClauses/Exceptions/Throw.cs
+++ Code/Light.GuardClauses/Exceptions/Throw.cs
@@ -91,6 +91,13 @@ namespace Light.GuardClauses.Exceptions
             throw new ArgumentOutOfRangeException(parameterName, message ?? $"{parameterName ?? "The value"} must not be less than or equal to {boundary}, but it actually is {parameter}.");
 
         /// <summary>
+        /// Throws the default <see cref="ArgumentOutOfRangeException" /> indicating that a comparable value must not be greater than or equal to the given boundary value, using the optional parameter name and message.
+        /// </summary>
+        [ContractAnnotation("=> halt")]
+        public static void MustNotBeGreaterThanOrEqualTo<T>(T parameter, T boundary, string parameterName = null, string message = null) where T : IComparable<T> =>
+            throw new ArgumentOutOfRangeException(parameterName, message ?? $"{parameterName ?? "The value"} must not be greater than or equal to {boundary}, but it actually is {parameter}.");
+
+        /// <summary>
         /// Throws the exception that is returned by <paramref name="exceptionFactory" />.
         /// </summary>
         [ContractAnnotation("=> halt")]
