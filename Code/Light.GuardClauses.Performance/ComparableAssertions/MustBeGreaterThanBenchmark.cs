7e8a4cc16c53152a19645da94727f271ca101423
Kenny Pflug <kenny.pflug@live.de>
2018-05-10 07:33:00 +0200

    added MustBeGreaterThan

---
+++ Code/Light.GuardClauses.Performance/ComparableAssertions/MustBeGreaterThanBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.ComparableAssertions
+{
+    public class MustBeGreaterThanBenchmark : DefaultBenchmark
+    {
+        public int First = 5000;
+        public int Second = -1939;
+
+        [Benchmark(Baseline = true)]
+        public int BaseVersion()
+        {
+            if (First <= Second) throw new ArgumentOutOfRangeException(nameof(First));
+            return First;
+        }
+
+        [Benchmark]
+        public int BaseVersionWithCompareTo()
+        {
+            if (First.CompareTo(Second) <= 0) throw new ArgumentOutOfRangeException(nameof(First));
+            return First;
+        }
+
+        [Benchmark]
+        public int LightGuardClausesWithParameterName() => First.MustBeGreaterThan(Second, nameof(First));
+
+        [Benchmark]
+        public int LightGuardClausesCustomException() => First.MustBeGreaterThan(Second, (p, b) => new Exception($"{p} is not less than {b}."));
+
+        [Benchmark]
+        public int OldVersion() => First.OldMustBeGreaterThan(Second, nameof(First));
+    }
+
+    public static class MustBeGreaterThanExtensionMethods
+    {
+        public static T OldMustBeGreaterThan<T>(this T parameter, T boundary, string parameterName = null, string message = null, Func<Exception> exception = null) where T : IComparable<T>
+        {
+            if (parameter.CompareTo(boundary) > 0)
+                return parameter;
+
+            throw exception != null ? exception() : new ArgumentOutOfRangeException(parameterName, parameter, message ?? $"{parameterName ?? "The value"} must be less than {boundary}, but you specified {parameter}.");
+        }
+    }
+}
---
+++ Code/Light.GuardClauses.Tests/ComparableAssertions/MustBeGreaterThanTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.ComparableAssertions
+{
+    public static class MustBeGreaterThanTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(-1, -1)]
+        [InlineData(0, 0)]
+        public static void ParameterEqualOrLess(int first, int second)
+        {
+            Action act = () => first.MustBeGreaterThan(second, nameof(first));
+
+            act.Should().Throw<ArgumentOutOfRangeException>()
+               .And.Message.Should().Contain($"{nameof(first)} must be greater than {second}, but it actually is {first}.");
+        }
+
+        [Theory]
+        [InlineData(133, 99)]
+        [InlineData(int.MaxValue, int.MaxValue - 1)]
+        [InlineData(byte.MaxValue, byte.MinValue)]
+        public static void ParameterGreater(int first, int second) => first.MustBeGreaterThan(second).Should().Be(first);
+
+        [Fact]
+        public static void CustomException() =>
+            Test.CustomException(40, 50, (x, y, exceptionFactory) => x.MustBeGreaterThan(y, exceptionFactory));
+
+        [Fact]
+        public static void NoCustomExceptionThrown() => 5.6m.MustBeGreaterThan(5.1m, (v, b) => null).Should().Be(5.6m);
+
+        [Fact]
+        public static void CustomMessage() =>
+            Test.CustomMessage<ArgumentOutOfRangeException>(message => 100.MustBeGreaterThan(100, message: message));
+
+    }
+}
