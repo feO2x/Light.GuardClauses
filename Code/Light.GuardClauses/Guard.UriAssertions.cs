551e6700bc4ac61da704153a5b8441ad603f2e9d
Kenny Pflug <kenny.pflug@live.de>
2018-06-06 21:28:07 +0200

    added MustBeHttpUrl

--- Code/Light.GuardClauses/Guard.UriAssertions.cs
+++ Code/Light.GuardClauses/Guard.UriAssertions.cs
@@ -139,5 +139,35 @@ namespace Light.GuardClauses
 #endif
         [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
         public static Uri MustBeHttpsUrl(this Uri parameter, Func<Uri, Exception> exceptionFactory, string parameterName = null) => parameter.MustHaveScheme("https", exceptionFactory, parameterName);
+
+        /// <summary>
+        /// Ensures that the specified URI has the "http" scheme, or otherwise throws an <see cref="InvalidUriSchemeException" />.
+        /// </summary>
+        /// <param name="parameter">The URI to be checked.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message that will be passed to the <see cref="InvalidUriSchemeException" /> (optional).</param>
+        /// <exception cref="InvalidUriSchemeException">Thrown when <paramref name="parameter" /> uses a different scheme than "http".</exception>
+        /// <exception cref="RelativeUriException">Thrown when <paramref name="parameter" /> is relative and thus has no scheme.</exception>
+        /// <exception cref="ArgumentNullException">Throw when <paramref name="parameter" /> is null.</exception>
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45 || SILVERLIGHT)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+        public static Uri MustBeHttpUrl(this Uri parameter, string parameterName = null, string message = null) => parameter.MustHaveScheme("http", parameterName, message);
+
+        /// <summary>
+        /// Ensures that the specified URI has the "http" scheme, or otherwise throws your custom exception.
+        /// </summary>
+        /// <param name="parameter">The URI to be checked.</param>
+        /// <param name="exceptionFactory">The delegate that creates the exception to be thrown.</param>
+        /// <param name="parameterName">The name of the parameter (optional). This is used for the <see cref="RelativeUriException"/> and <see cref="ArgumentNullException"/>.</param>
+        /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> uses a different scheme than "http".</exception>
+        /// <exception cref="RelativeUriException">Thrown when <paramref name="parameter" /> is relative and thus has no scheme.</exception>
+        /// <exception cref="ArgumentNullException">Throw when <paramref name="parameter" /> is null.</exception>
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45 || SILVERLIGHT)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+        public static Uri MustBeHttpUrl(this Uri parameter, Func<Uri, Exception> exceptionFactory, string parameterName = null) => parameter.MustHaveScheme("http", exceptionFactory, parameterName);
     }
 }
