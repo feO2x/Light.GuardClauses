0b52cb39454b755445efb3baa66e437e7906d985
Kenny Pflug <kenny.pflug@live.de>
2018-06-10 08:25:55 +0200

    added tests and benchmark for MustContain

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/MustContainBenchmark.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Globalization;
+using BenchmarkDotNet.Attributes;
+using Light.GuardClauses.Exceptions;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class MustContainBenchmark : DefaultBenchmark
+    {
+        public string LongString = "A mind needs books as a sword needs a whetstone, if it is to keep its edge.";
+        public string ShortString = "Edge";
+        public string Substring = "dge";
+
+        [Benchmark(Baseline = true)]
+        public string ImperativeVersionShort()
+        {
+            if (ShortString == null) throw new ArgumentNullException(nameof(ShortString));
+            if (!ShortString.Contains(Substring)) throw new SubstringException(nameof(ShortString));
+
+            return ShortString;
+        }
+
+        [Benchmark]
+        public string ImperativeVersionLong()
+        {
+            if (LongString == null) throw new ArgumentNullException(nameof(LongString));
+            if (!LongString.Contains(Substring)) throw new SubstringException(nameof(LongString));
+
+            return LongString;
+        }
+
+        [Benchmark]
+        public string LightGuardClausesShort() => ShortString.MustContain(Substring, nameof(ShortString));
+
+        [Benchmark]
+        public string LightGuardClausesShortCustomException() => ShortString.MustContain(Substring, (s1, s2) => new Exception("Not contained."));
+
+        [Benchmark]
+        public string LightGuardClausesLong() => LongString.MustContain(Substring, nameof(LongString));
+
+        [Benchmark]
+        public string LightGuardClausesLongCustomException() => LongString.MustContain(Substring, (s1, s2) => new Exception("Not contained."));
+
+        [Benchmark]
+        public string LightGuardClausesShortCustomSearch() => ShortString.MustContain(Substring, StringComparison.OrdinalIgnoreCase, nameof(ShortString));
+
+        [Benchmark]
+        public string LightGuardClausesShortCustomSearchCustomException() => ShortString.MustContain(Substring, StringComparison.OrdinalIgnoreCase, (s1, s2, c) => new Exception("Not contained."));
+
+        [Benchmark]
+        public string LightGuardClausesLongCustomSearch() => LongString.MustContain(Substring, StringComparison.OrdinalIgnoreCase, nameof(ShortString));
+
+        [Benchmark]
+        public string LightGuardClausesLongCustomSearchCustomException() => LongString.MustContain(Substring, StringComparison.OrdinalIgnoreCase, (s1, s2, c) => new Exception("Not contained."));
+
+        [Benchmark]
+        public string OldVersionShort() => ShortString.OldMustContain(Substring, parameterName: nameof(ShortString));
+
+        [Benchmark]
+        public string OldVersionLong() => LongString.OldMustContain(Substring, parameterName: nameof(LongString));
+
+        [Benchmark]
+        public string OldVersionShortCustomSearch() => ShortString.OldMustContain(Substring, true, nameof(ShortString));
+
+        [Benchmark]
+        public string OldVersionLOngCustomSearch() => LongString.OldMustContain(Substring, true, nameof(LongString));
+    }
+
+    public static class MustContainExtensions
+    {
+        public static string OldMustContain(this string parameter, string text, IgnoreCaseInfo ignoreCase = default, string parameterName = null, string message = null, Func<Exception> exception = null)
+        {
+            parameter.MustNotBeNull(parameterName);
+            text.MustNotBeNull(nameof(text));
+
+            if (parameter.IsContaining(text, ignoreCase)) return parameter;
+
+            throw exception?.Invoke() ?? new StringException(message ?? $"\"{parameter}\" must contain \"{text}\", but it does not.", parameterName);
+        }
+
+        public static bool IsContaining(this string @string, string text, IgnoreCaseInfo ignoreCase = default)
+        {
+            @string.MustNotBeNull(nameof(@string));
+            text.MustNotBeNull(nameof(text));
+
+            return ignoreCase.StringContains(@string, text);
+        }
+
+        public readonly struct IgnoreCaseInfo
+        {
+            public readonly CultureInfo CultureInfo;
+
+            public readonly CompareOptions CompareOptions;
+
+            public IgnoreCaseInfo(CultureInfo cultureInfo, CompareOptions compareOptions)
+            {
+                CultureInfo = cultureInfo.MustNotBeNull(nameof(cultureInfo));
+                CompareOptions = compareOptions.MustBeValidEnumValue(nameof(compareOptions));
+            }
+
+            public static implicit operator IgnoreCaseInfo(bool ignoreCase)
+            {
+                return ignoreCase ? new IgnoreCaseInfo(CultureInfo.InvariantCulture, CompareOptions.OrdinalIgnoreCase) : new IgnoreCaseInfo();
+            }
+
+            public static implicit operator IgnoreCaseInfo(CultureInfo cultureInfo)
+            {
+                return new IgnoreCaseInfo(cultureInfo, CompareOptions.OrdinalIgnoreCase);
+            }
+
+            public bool StringContains(string @string, string substring)
+            {
+                if (CultureInfo == null)
+                    return @string.MustNotBeNull(nameof(@string)).Contains(substring);
+
+                return CultureInfo.CompareInfo.IndexOf(@string, substring, CompareOptions) != -1;
+            }
+        }
+    }
+}
---
+++ Code/Light.GuardClauses.Tests/StringAssertions/MustContainTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Exceptions;
+using Light.GuardClauses.FrameworkExtensions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.StringAssertions
+{
+    public static class MustContainTests
+    {
+        [Theory]
+        [InlineData("Foo", "Bar")]
+        [InlineData("abc", "d")]
+        [InlineData("Hello, World!", "You")]
+        [InlineData("1, 2, 3", ". ")]
+        public static void StringDoesNotContain(string @string, string other)
+        {
+            Action act = () => @string.MustContain(other, nameof(@string));
+
+            act.Should().Throw<SubstringException>()
+               .And.Message.Should().Contain($"{nameof(@string)} must contain {other.ToStringOrNull()}, but it actually is {@string.ToStringOrNull()}.");
+        }
+
+        [Theory]
+        [InlineData("Foo", "oo")]
+        [InlineData("Bar", "Bar")]
+        [InlineData("Baz", "B")]
+        [InlineData("abc", "a")]
+        [InlineData("Hello, World!", "orl")]
+        [InlineData("1, 2, 3", ", ")]
+        public static void StringContains(string @string, string substring) =>
+            @string.MustContain(substring).Should().BeSameAs(@string);
+
+        [Theory]
+        [InlineData("Why are all the gods such vicious cunts ? Where is the god of tits and wine?", "beer", StringComparison.OrdinalIgnoreCase)]
+        [InlineData("I'm the captain. If this ship goes down, I go down with it.", "SHIP", StringComparison.Ordinal)]
+        public static void StringDoesNotContainCustomSearch(string @string, string other, StringComparison comparisonType)
+        {
+            Action act = () => @string.MustContain(other, comparisonType, nameof(@string));
+
+            act.Should().Throw<SubstringException>()
+               .And.Message.Should().Contain($"{nameof(@string)} must contain {other.ToStringOrNull()} ({comparisonType}), but it actually is {@string.ToStringOrNull()}.");
+        }
+
+        [Theory]
+        [InlineData("Foo", "foo", StringComparison.OrdinalIgnoreCase)]
+        [InlineData("I suppose I'll have to kill the Mountain myself. Won't that make for a great song.", "KILL", StringComparison.OrdinalIgnoreCase)]
+        public static void StringContainsCustomSearch(string @string, string substring, StringComparison comparisonType) => 
+            @string.MustContain(substring, comparisonType).Should().BeSameAs(@string);
+
+        [Fact]
+        public static void StringNull()
+        {
+            Action act = () => ((string) null).MustContain(Metasyntactic.Foo);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public static void StringNullCustomSearch()
+        {
+            Action act = () => ((string)null).MustContain(Metasyntactic.Foo, StringComparison.Ordinal);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public static void CustomException() => 
+            Test.CustomException(Metasyntactic.Foo,
+                                 Metasyntactic.Bar,
+                                 (@string, other, exceptionFactory) => @string.MustContain(other, exceptionFactory));
+
+        [Fact]
+        public static void CustomExceptionCustomSearch() => 
+            Test.CustomException("Foo",
+                                 "foo",
+                                 StringComparison.Ordinal,
+                                 (@string, other, comparisonType, exceptionFactory) => @string.MustContain(other, comparisonType, exceptionFactory));
+
+        [Fact]
+        public static void CustomMessage() => 
+            Test.CustomMessage<SubstringException>(message => Metasyntactic.Foo.MustContain(Metasyntactic.Bar, message: message));
+
+        [Fact]
+        public static void CustomMessageCustomSearch() => 
+            Test.CustomMessage<SubstringException>(message => Metasyntactic.Baz.MustContain(Metasyntactic.Qux, StringComparison.OrdinalIgnoreCase, message: message));
+    }
+}
