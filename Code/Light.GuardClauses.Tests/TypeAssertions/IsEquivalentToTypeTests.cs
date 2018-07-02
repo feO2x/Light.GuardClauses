3edd3842721cd4bbe9d647f1acf73a97a789fef1
Kenny Pflug <kenny.pflug@live.de>
2018-07-02 13:28:34 +0200

    added IsEquivalentTypeTo

---
+++ Code/Light.GuardClauses.Tests/TypeAssertions/IsEquivalentToTypeTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.TypeAssertions
+{
+    public static class IsEquivalentTypeToTests
+    {
+        [Theory]
+        [InlineData(typeof(string), typeof(string), true)]
+        [InlineData(typeof(double), typeof(double), true)]
+        [InlineData(typeof(int), typeof(object), false)]
+        [InlineData(typeof(ArgumentException), typeof(Exception), false)]
+        [InlineData(null, null, true)]
+        [InlineData(null, typeof(object), false)]
+        [InlineData(typeof(object), null, false)]
+        public static void NonGenericTypes(Type type, Type other, bool expected) => 
+            type.IsEquivalentTypeTo(other).Should().Be(expected);
+
+        [Fact]
+        public static void GenericTypeDefinition() => 
+            TestEquivalence(typeof(List<>), typeof(List<>), true);
+
+        [Fact]
+        public static void ClosedConstructedGenericType() => 
+            TestEquivalence(typeof(IList<string>), typeof(IList<string>), true);
+
+        [Fact]
+        public static void UnequalClosedConstructedGenericType() => 
+            TestEquivalence(typeof(IReadOnlyList<string>), typeof(IReadOnlyList<object>), false);
+
+        [Fact]
+        public static void OpenConstructedGenericTypeAndGenericTypeDefinition() => 
+            TestEquivalence(typeof(SubTypeA<>).GetTypeInfo().BaseType, typeof(GenericType<>), true);
+
+        [Fact]
+        public static void ClosedConstructedBaseType() => 
+            TestEquivalence(typeof(SubTypeB).GetTypeInfo().BaseType, typeof(GenericType<string>), true);
+
+        [Fact]
+        public static void ClosedConstructedBaseTypeAndGenericTypeDefinition() => 
+            TestEquivalence(typeof(SubTypeA<int>).GetTypeInfo().BaseType, typeof(GenericType<>), true);
+
+        [Fact]
+        public static void DifferentTypes() => 
+            TestEquivalence(typeof(SubTypeA<int>).GetTypeInfo().BaseType, typeof(List<>), false);
+
+        [Fact]
+        public static void OpenConstructedReturnTypeAndGenericTypeDefinition() => 
+            TestEquivalence(CreateDictionaryOfTKeyTValue.ReturnType, typeof(Dictionary<,>), true);
+
+        [Fact]
+        public static void PartiallyConstructedReturnTypeAndGenericTypeDefinition() => 
+            TestEquivalence(CreateDictionaryOfTKey.ReturnType, typeof(Dictionary<,>), true);
+
+        [Fact]
+        public static void DifferentConstructedGenericTypes() => 
+            TestEquivalence(CreateDictionaryOfTKeyTValue.ReturnType, CreateDictionaryOfTKey.ReturnType, false);
+
+        private static void TestEquivalence(Type first, Type second, bool expected)
+        {
+            first.IsEquivalentTypeTo(second).Should().Be(expected);
+            second.IsEquivalentTypeTo(first).Should().Be(expected);
+        }
+
+        public class GenericType<T> { }
+
+        public class SubTypeA<T> : GenericType<T> { }
+
+        public class SubTypeB : GenericType<string> { }
+
+        public static Dictionary<TKey, TValue> CreateDictionaryA<TKey, TValue>() => 
+            new Dictionary<TKey, TValue>();
+
+        public static Dictionary<TKey, object> CreateDictionaryB<TKey>() => 
+            new Dictionary<TKey, object>();
+
+        private static readonly MethodInfo CreateDictionaryOfTKeyTValue;
+        private static readonly MethodInfo CreateDictionaryOfTKey;
+
+        static IsEquivalentTypeToTests()
+        {
+            var typeInfo = typeof(IsEquivalentTypeToTests).GetTypeInfo();
+            CreateDictionaryOfTKeyTValue = typeInfo.DeclaredMethods.First(m => m.IsStatic && m.Name.StartsWith(nameof(CreateDictionaryA)));
+            CreateDictionaryOfTKey = typeInfo.DeclaredMethods.First(m => m.IsStatic && m.Name.StartsWith(nameof(CreateDictionaryB)));
+        }
+    }
+}
---
+++ Code/Light.GuardClauses/Guard.TypeAssertions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45 || SILVERLIGHT)
+using System.Runtime.CompilerServices;
+#endif
+
+namespace Light.GuardClauses
+{
+    public static partial class Guard
+    {
+        /// <summary>
+        /// Checks if the two specified types are equivalent. This is true when both types are equal or
+        /// when one type is a constructed generic type and the other type is the corresponding generic type definition.
+        /// </summary>
+        /// <param name="type">The first type to be checked.</param>
+        /// <param name="other">The other type to be checked.</param>
+        /// <returns>
+        /// True if both types are null, or if both are equal, or if one type
+        /// is a constructed generic type and the other one is the corresponding generic type definition, else false.
+        /// </returns>
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45 || SILVERLIGHT)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        public static bool IsEquivalentTypeTo(this Type type, Type other) =>
+            ReferenceEquals(type, other) ||
+            !(type is null) &&
+            other != null &&
+            (type == other ||
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45)
+             type.IsConstructedGenericType != other.IsConstructedGenericType &&
+#else
+             type.IsConstructedGenericType() != other.IsConstructedGenericType() &&
+#endif
+             CheckTypeEquivalency(type, other));
+
+        private static bool CheckTypeEquivalency(Type type, Type other)
+        {
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45)
+            if (type.IsConstructedGenericType)
+#else
+			if (type.IsConstructedGenericType())
+#endif
+                return type.GetGenericTypeDefinition() == other;
+            return other.GetGenericTypeDefinition() == type;
+        }
+
+#if (NET35 || NET40 || SILVERLIGHT)
+/// <summary>
+/// Gets a value that indicates whether this object represents a constructed generic type. You can create instances of a constructed generic type.
+/// </summary>
+/// <param name="type">The type to be checked.</param>
+/// <returns>True if this object represents a constructed generic type, else false.</returns>
+        public static bool IsConstructedGenericType(this Type type)
+        {
+            if (type == null)
+                return false;
+
+            return type.IsGenericType && !type.ContainsGenericParameters;
+        }
+#endif
+    }
+}
