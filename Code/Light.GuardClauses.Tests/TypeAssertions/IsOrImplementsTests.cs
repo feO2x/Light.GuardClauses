d351ef8977877f583c46970fd2279d651d50297c
Kenny Pflug <kenny.pflug@live.de>
2018-07-02 18:14:35 +0200

    added IsOrImplements

---
+++ Code/Light.GuardClauses.Tests/TypeAssertions/IsOrImplementsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.TypeAssertions
+{
+    public static class IsOrImplementsTests
+    {
+        [Fact]
+        public static void BasicFunctionality()
+        {
+            CheckIsOrImplements(typeof(string), typeof(string), true);
+            CheckIsOrImplements(typeof(string), typeof(object), false);
+            CheckIsOrImplements(typeof(string), typeof(IComparable<string>), true);
+            CheckIsOrImplements(typeof(ICollection<string>), typeof(ICollection<string>), true);
+            CheckIsOrImplements(typeof(ICollection<string>), typeof(ICollection<>), true);
+            CheckIsOrImplements(typeof(ICollection<string>), typeof(IEnumerable<>), true);
+            CheckIsOrImplements(typeof(ICollection<string>), typeof(IList<string>), false);
+            CheckIsOrImplements(typeof(ICollection<string>), typeof(IList<>), false);
+        }
+
+        [Fact]
+        public static void TypeNull()
+        {
+            var type = default(Type);
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action act = () => type.IsOrImplements(typeof(object));
+
+            act.Should().Throw<ArgumentNullException>()
+               .And.ParamName.Should().Be(nameof(type));
+        }
+
+        [Fact]
+        public static void OtherTypeNull()
+        {
+            var otherType = default(Type);
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action act = () => typeof(object).IsOrImplements(otherType);
+
+            act.Should().Throw<ArgumentNullException>()
+               .And.ParamName.Should().Be(nameof(otherType));
+        }
+
+        private static void CheckIsOrImplements(Type type, Type otherType, bool expected) =>
+            type.IsOrImplements(otherType).Should().Be(expected);
+    }
+}
--- Code/Light.GuardClauses/Guard.TypeAssertions.cs
+++ Code/Light.GuardClauses/Guard.TypeAssertions.cs
@@ -121,6 +121,37 @@ namespace Light.GuardClauses
         }
 
         /// <summary>
+        /// Checks if the given <paramref name="type" /> is equal to the specified <paramref name="otherType" /> or if it implements it. Internally, this
+        /// method uses <see cref="IsEquivalentTypeTo" /> so that constructed generic types and their corresponding generic type defintions are regarded as equal.
+        /// </summary>
+        /// <param name="type">The type to be checked.</param>
+        /// <param name="otherType">The type that is equivalent to <paramref name="type" /> or the interface type that <paramref name="type" /> implements.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="type" /> or <paramref name="otherType" /> is null.</exception>
+        /// <returns>True if <paramref name="type" /> is equivalent to or implements <paramref name="otherType" /> directly or indirectly, else false.</returns>
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45 || SILVERLIGHT)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        [ContractAnnotation("type:null => halt; otherType:null => halt")]
+        public static bool IsOrImplements(this Type type, Type otherType) => 
+            type.IsEquivalentTypeTo(otherType.MustNotBeNull(nameof(otherType))) || type.Implements(otherType);
+
+        /// <summary>
+        /// Checks if the given <paramref name="type" /> is equal to the specified <paramref name="otherType" /> or if it implements it. This overload uses the specified <paramref name="typeComparer"/>
+        /// to compare the types.
+        /// </summary>
+        /// <param name="type">The type to be checked.</param>
+        /// <param name="otherType">The type that is equivalent to <paramref name="type" /> or the interface type that <paramref name="type" /> implements.</param>
+        /// <param name="typeComparer">The equality comparer used to compare the interface types.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="type" /> or <paramref name="otherType" /> is null.</exception>
+        /// <returns>True if <paramref name="type" /> is equivalent to or implements <paramref name="otherType" /> directly or indirectly, else false.</returns>
+#if (NETSTANDARD2_0 || NETSTANDARD1_0 || NET45 || SILVERLIGHT)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+        [ContractAnnotation("type:null => halt; otherType:null => halt")]
+        public static bool IsOrImplements(this Type type, Type otherType, IEqualityComparer<Type> typeComparer) =>
+            typeComparer.MustNotBeNull(nameof(typeComparer)).Equals(type.MustNotBeNull(nameof(type)), otherType.MustNotBeNull(nameof(otherType))) || type.Implements(otherType, typeComparer);
+
+        /// <summary>
         /// Checks if the specified type derives from the other type. Internally, this method uses <see cref="IsEquivalentTypeTo" />
         /// by default so that constructed generic types and their corresponding generic type definitions are regarded as equal.
         /// </summary>
