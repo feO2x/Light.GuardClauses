0f3ac5c3faf1bec85b4e49ba7460153b3c2d7a84
Kenny Pflug <kenny.pflug@live.de>
2018-07-07 15:00:23 +0200

    added tests for AsArray

---
+++ Code/Light.GuardClauses.Tests/FrameworkExtensions/AsArrayTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using FluentAssertions;
+using Light.GuardClauses.FrameworkExtensions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.FrameworkExtensions
+{
+    public static class AsArrayTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 })]
+        [InlineData(new[] { 89, 87 })]
+        [InlineData(new int[] { })]
+        public static void DowncastArray(int[] array)
+        {
+            var result = array.AsArray();
+
+            result.Should().BeSameAs(array);
+        }
+
+        [Theory]
+        [MemberData(nameof(NotArrayData))]
+        public static void CreateNewArray(IEnumerable<string> enumerable)
+        {
+            // ReSharper disable PossibleMultipleEnumeration
+            var result = enumerable.AsArray();
+
+            result.Should().NotBeSameAs(enumerable);
+            result.Length.Should().Be(enumerable.Count());
+            result.Should().Equal(enumerable);
+            // ReSharper restore PossibleMultipleEnumeration
+        }
+
+        [Fact]
+        public static void EnumerableNull()
+        {
+            Action act = () => ((ObservableCollection<string>) null).AsArray();
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        public static readonly TheoryData<IEnumerable<string>> NotArrayData =
+            new TheoryData<IEnumerable<string>>
+            {
+                new List<string> { "Foo", "Bar", "Baz" },
+                new ObservableCollection<string>(),
+                LazyStringCollection
+            };
+
+        public static IEnumerable<string> LazyStringCollection
+        {
+            get
+            {
+                yield return "Foo";
+                yield return "Baz";
+                yield return "Quux";
+            }
+        }
+    }
+}
