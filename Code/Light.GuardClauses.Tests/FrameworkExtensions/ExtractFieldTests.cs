87c14f42876d421a0a38c6050ee676285e45c725
Kenny Pflug <kenny.pflug@live.de>
2018-07-08 10:15:16 +0200

    added ExtractProperty and ExtractField

---
+++ Code/Light.GuardClauses.Tests/FrameworkExtensions/ExtractFieldTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using FluentAssertions;
+using Light.GuardClauses.FrameworkExtensions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.FrameworkExtensions
+{
+    public static class ExtractFieldTests
+    {
+        [Fact]
+        public static void ExtractPropertyInfo()
+        {
+            Expression<Func<Foo, int>> expression = foo => foo.Baz;
+
+            var fieldInfo = expression.ExtractField();
+
+            fieldInfo.Should().BeSameAs(typeof(Foo).GetTypeInfo().GetField(nameof(Foo.Baz)));
+        }
+
+        [Fact]
+        public static void InvalidExpression()
+        {
+            Expression<Func<Foo, string>> expression = foo => foo.Bar;
+
+            Action act = () => expression.ExtractField();
+
+            act.Should().Throw<ArgumentException>()
+               .And.Message.Should().Contain("The specified expression is not valid. Please use an expression like the following one: o => o.Field");
+        }
+
+        [Fact]
+        public static void ExpressionNull()
+        {
+            Action act = () => ((Expression<Func<object, object>>) null).ExtractField();
+
+            act.Should().Throw<ArgumentNullException>()
+               .And.ParamName.Should().Be("expression");
+        }
+    }
+}
---
+++ Code/Light.GuardClauses.Tests/FrameworkExtensions/ExtractPropertyTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using FluentAssertions;
+using Light.GuardClauses.FrameworkExtensions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.FrameworkExtensions
+{
+    public static class ExtractPropertyTests
+    {
+        [Fact]
+        public static void ExtractPropertyInfo()
+        {
+            Expression<Func<Foo, string>> expression = foo => foo.Bar;
+
+            var propertyInfo = expression.ExtractProperty();
+
+            propertyInfo.Should().BeSameAs(typeof(Foo).GetTypeInfo().GetProperty(nameof(Foo.Bar)));
+        }
+
+        [Fact]
+        public static void InvalidExpression()
+        {
+            Expression<Func<Foo, int>> expression = foo => foo.Baz;
+
+            Action act = () => expression.ExtractProperty();
+
+            act.Should().Throw<ArgumentException>()
+               .And.Message.Should().Contain("The specified expression is not valid. Please use an expression like the following one: o => o.Property");
+        }
+
+
+        [Fact]
+        public static void ExpressionNull()
+        {
+            Action act = () => ((Expression<Func<object, object>>) null).ExtractProperty();
+
+            act.Should().Throw<ArgumentNullException>()
+               .And.ParamName.Should().Be("expression");
+        }
+    }
+
+    public class Foo
+    {
+        public int Baz;
+        public string Bar { get; set; }
+    }
+}
