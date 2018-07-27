d6cf01bd5052bd593f37c1a81cd49ed216c66a6b
Kenny Pflug <kenny.pflug@live.de>
2018-07-27 20:47:32 +0200

    added custom exception and custom message tests for MustBeGreaterThanOrEqualTo

--- Code/Light.GuardClauses.Tests/ComparableAssertions/MustBeGreaterThanOrEqualToTests.cs
+++ Code/Light.GuardClauses.Tests/ComparableAssertions/MustBeGreaterThanOrEqualToTests.cs
@@ -14,8 +14,9 @@ namespace Light.GuardClauses.Tests.ComparableAssertions
         {
             Action act = () => value.MustNotBeGreaterThanOrEqualTo(boundary, nameof(value));
 
-            act.Should().Throw<ArgumentOutOfRangeException>()
-               .And.Message.Should().Contain($"{nameof(value)} must not be greater than or equal to {boundary}, but it actually is {value}.");
+            var exceptionAssertion = act.Should().Throw<ArgumentOutOfRangeException>().Which;
+            exceptionAssertion.Message.Should().Contain($"{nameof(value)} must not be greater than or equal to {boundary}, but it actually is {value}.");
+            exceptionAssertion.ParamName.Should().BeSameAs(nameof(value));
         }
 
         [Theory]
@@ -28,10 +29,20 @@ namespace Light.GuardClauses.Tests.ComparableAssertions
             Test.CustomException('A', 'G', (a, g, exceptionFactory) => a.MustBeGreaterThanOrEqualTo(g, exceptionFactory));
 
         [Fact]
+        public static void CustomExceptionParameterNull() => 
+            Test.CustomException((string) null,
+                                 Metasyntactic.Foo,
+                                 (x, y, exceptionFactory) => x.MustBeGreaterThanOrEqualTo(y, exceptionFactory));
+
+        [Fact]
         public static void NoCustomExceptionThrown() => 5m.MustNotBeGreaterThanOrEqualTo(5.1m, (v, b) => null).Should().Be(5m);
 
         [Fact]
         public static void CustomMessage() =>
             Test.CustomMessage<ArgumentOutOfRangeException>(message => 99.MustBeGreaterThanOrEqualTo(100, message: message));
+
+        [Fact]
+        public static void CustomMessageParamterNull() => 
+            Test.CustomMessage<ArgumentNullException>(message => ((string) null).MustBeGreaterThanOrEqualTo(Metasyntactic.Bar, message: message));
     }
 }
