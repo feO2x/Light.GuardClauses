71cdfcfefe3a602f6a77405a539d36436e8a2a81
Kenny Pflug <kenny.pflug@live.de>
2018-11-10 12:34:41 +0100

    added string.MustBe overload that accepts a StringComparisonType parameter

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/MustBeBenchmark.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Light.GuardClauses.Exceptions;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class MustBeBenchmark : DefaultBenchmark
+    {
+        public string X = "{\"foo\":42}";
+        public string Y = "{\r\n  \"foo\": 42\r\n}\r\n";
+
+        [Benchmark(Baseline = true)]
+        public string ImperativeVersion()
+        {
+            if (!X.Equals(Y, StringComparisonType.OrdinalIgnoreWhiteSpace))
+                throw new ValuesNotEqualException(nameof(X));
+            return X;
+        }
+
+        [Benchmark]
+        public string LightGuardClauses() => X.MustBe(Y, StringComparisonType.OrdinalIgnoreWhiteSpace);
+
+        [Benchmark]
+        public string LightGuardClausesCustomException() => 
+            X.MustBe(Y, StringComparisonType.OrdinalIgnoreWhiteSpace, (x, y) => new Exception("The strings are not equal."));
+    }
+}
--- Code/Light.GuardClauses/StringComparisonType.cs
+++ Code/Light.GuardClauses/StringComparisonType.cs
@@ -3,6 +3,12 @@
     /// <summary>
     /// Specifies the culture, case , and sort rules when comparing strings.
     /// </summary>
+    /// <remarks>
+    /// This enum is en extension of <see cref="System.StringComparison"/>, adding
+    /// capabilities to ignore white space when making string equality comparisons.
+    /// See the <see cref="Check.Equals(string,string,StringComparisonType)"/> when
+    /// you want to compare in such a way.
+    /// </remarks>
     public enum StringComparisonType
     {
         /// <summary>
