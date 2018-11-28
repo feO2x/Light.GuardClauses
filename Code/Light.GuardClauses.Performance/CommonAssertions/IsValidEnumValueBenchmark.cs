31a333df7deee5801b89cc420c5f8727c1f55af7
Kenny Pflug <kenny.pflug@live.de>
2018-11-28 20:55:00 +0100

    added IsValidEnumValueBenchmark

---
+++ Code/Light.GuardClauses.Performance/CommonAssertions/IsValidEnumValueBenchmark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.CommonAssertions
+{
+    public class IsValidEnumValueBenchmark
+    {
+        public AttributeTargets AttributeTargets = AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface;
+        public StringComparison ComparisonType = StringComparison.Ordinal;
+
+        [Benchmark(Baseline = true)]
+        public bool Imperative() => Enum.IsDefined(typeof(StringComparison), ComparisonType);
+
+        [Benchmark]
+        public bool LightGuardClauses() => ComparisonType.IsValidEnumValue();
+
+        [Benchmark]
+        public bool LightGuardClausesFlags() => AttributeTargets.IsValidEnumValue();
+    }
+}
