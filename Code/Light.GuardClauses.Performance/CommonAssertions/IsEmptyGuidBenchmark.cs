1dcb4eb110d88a9564f621ef5a77b1033bd1d45f
Kenny Pflug <kenny.pflug@live.de>
2018-11-28 20:58:54 +0100

    added IsEmptyGuidBenchmark

---
+++ Code/Light.GuardClauses.Performance/CommonAssertions/IsEmptyGuidBenchmark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.CommonAssertions
+{
+    public class IsEmptyGuidBenchmark
+    {
+        public Guid Guid = Guid.NewGuid();
+
+        [Benchmark(Baseline = true)]
+        public bool Imperative() => Guid == Guid.Empty;
+
+        [Benchmark]
+        public bool LightGuardClauses() => Guid.IsEmpty();
+    }
+}
