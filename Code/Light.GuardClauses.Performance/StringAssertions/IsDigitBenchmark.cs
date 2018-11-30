c31571aa9ebdd65b4ca027cf332fd326139ccab0
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 10:19:57 +0100

    added benchmark for IsDigit

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/IsDigitBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class IsDigitBenchmark
+    {
+        public char Digit = '9';
+        public char NoDigit = 't';
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeDigit() => char.IsDigit(Digit);
+
+        [Benchmark]
+        public bool ImperativeNoDigit() => char.IsDigit(NoDigit);
+
+        [Benchmark]
+        public bool LightGuardClausesDigit() => Digit.IsDigit();
+
+        [Benchmark]
+        public bool LightGuardClausesNoDigit() => NoDigit.IsDigit();
+    }
+}
