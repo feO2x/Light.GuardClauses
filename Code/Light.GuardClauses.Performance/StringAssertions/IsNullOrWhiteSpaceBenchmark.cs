ed4739161ec3808fe9ac1d17aca81ef65557c0d0
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 10:07:08 +0100

    added benchmark for IsNullOrWhiteSpace

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/IsNullOrWhiteSpaceBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class IsNullOrWhiteSpaceBenchmark
+    {
+        public string EmptyString = "";
+        public string NullString = null;
+        public string StringWithContent = "What`s up";
+        public string WhiteSpaceString = "  \t\r\n";
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeNull() => string.IsNullOrWhiteSpace(NullString);
+
+        [Benchmark]
+        public bool ImperativeEmpty() => string.IsNullOrWhiteSpace(EmptyString);
+
+        [Benchmark]
+        public bool ImperativeWhiteSpace() => string.IsNullOrWhiteSpace(WhiteSpaceString);
+
+        [Benchmark]
+        public bool ImperativeStringWithContent() => string.IsNullOrWhiteSpace(StringWithContent);
+
+        [Benchmark]
+        public bool LightGuardClausesNull() => NullString.IsNullOrWhiteSpace();
+
+        [Benchmark]
+        public bool LightGuardClausesEmpty() => EmptyString.IsNullOrWhiteSpace();
+
+        [Benchmark]
+        public bool LightGuardClausesWhiteSpaceString() => WhiteSpaceString.IsNullOrWhiteSpace();
+
+        [Benchmark]
+        public bool LightGuardClausesStringWithContent() => StringWithContent.IsNullOrWhiteSpace();
+    }
+}
