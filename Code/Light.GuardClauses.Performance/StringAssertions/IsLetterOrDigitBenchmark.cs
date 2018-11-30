c68d65896dc2fbdd68bdc47c0c85d66b99608bc6
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 10:17:39 +0100

    added benchmark for IsLetterOrDigit

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/IsLetterOrDigitBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class IsLetterOrDigitBenchmark
+    {
+        public char Digit = '4';
+        public char Letter = 'P';
+        public char NoLetterOrDigit = '/';
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeLetter() => char.IsLetterOrDigit(Letter);
+
+        [Benchmark]
+        public bool ImperativeDigit() => char.IsLetterOrDigit(Digit);
+
+        [Benchmark]
+        public bool ImperativeNoLetterOrDigit() => char.IsLetterOrDigit(NoLetterOrDigit);
+
+        [Benchmark]
+        public bool LightGuardClausesLetter() => Letter.IsLetterOrDigit();
+
+        [Benchmark]
+        public bool LightGuardClausesDigit() => Digit.IsLetterOrDigit();
+
+        [Benchmark]
+        public bool LightGuardClausesNoLetterOrDigit() => NoLetterOrDigit.IsLetterOrDigit();
+    }
+}
