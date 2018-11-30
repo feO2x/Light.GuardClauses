04d0b50758dd8924a1974b60a0686d4475ac7d18
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 08:56:37 +0100

    added benchmark for IsNotIn

---
+++ Code/Light.GuardClauses.Performance/ComparableAssertions/IsNotInBenchmark.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.ComparableAssertions
+{
+    public class IsNotInBenchmark
+    {
+        public const char LowerBoundary = 'A';
+        public const char UpperBoundary = 'G';
+        public Range<char> ExistingRange = Range.FromInclusive(LowerBoundary).ToInclusive(UpperBoundary);
+        public char Value = 'I';
+
+        [Benchmark(Baseline = true)]
+        public bool Imperative() => Value < ExistingRange.From || Value > ExistingRange.To;
+
+        [Benchmark]
+        public bool LightGuardClausesNewRangeInstance() => Value.IsNotIn(Range.FromInclusive(LowerBoundary).ToInclusive(UpperBoundary));
+
+        [Benchmark]
+        public bool LightGuardClausesExistingRangeInstance() => Value.IsNotIn(ExistingRange);
+    }
+}
