bccd16538344fbbc8372aa3a19d9a0b01f64d8b0
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 10:13:38 +0100

    added benchmark for IsLetter

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/IsLetterBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class IsLetterBenchmark
+    {
+        public char Letter = 'w';
+        public char NoLetter = '%';
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeLetter() => char.IsLetter(Letter);
+
+        [Benchmark]
+        public bool ImperativeNoLetter() => char.IsLetter(NoLetter);
+
+        [Benchmark]
+        public bool LightGuardClausesLetter() => Letter.IsLetter();
+
+        [Benchmark]
+        public bool LightGuardClausesNoLetter() => NoLetter.IsLetter();
+    }
+}
