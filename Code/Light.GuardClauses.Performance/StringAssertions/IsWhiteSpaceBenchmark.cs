3c1b48ad60ef7dfe02e10078832b40b27c0f5667
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 10:10:26 +0100

    added benchmark for IsWhiteSpace

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/IsWhiteSpaceBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class IsWhiteSpaceBenchmark
+    {
+        public char NoWhiteSpace = 'K';
+        public char WhiteSpace = '\t';
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeWhiteSpace() => char.IsWhiteSpace(WhiteSpace);
+
+        [Benchmark]
+        public bool ImperativeNoWhiteSpace() => char.IsWhiteSpace(NoWhiteSpace);
+
+        [Benchmark]
+        public bool LightGuardClausesWhiteSpace() => WhiteSpace.IsWhiteSpace();
+
+        [Benchmark]
+        public bool LightGuardClausesNoWhiteSpace() => NoWhiteSpace.IsWhiteSpace();
+    }
+}
