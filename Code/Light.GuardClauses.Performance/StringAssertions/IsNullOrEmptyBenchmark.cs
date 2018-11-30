b081ec4a0ce660754e2348c161015d5d9c41469c
Kenny Pflug <kenny.pflug@live.de>
2018-11-30 09:31:08 +0100

    added benchmark for IsNullOrEmpty

---
+++ Code/Light.GuardClauses.Performance/StringAssertions/IsNullOrEmptyBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Light.GuardClauses.Performance.StringAssertions
+{
+    public class IsNullOrEmptyBenchmark
+    {
+        public string EmptyString = "";
+        public string NullString = null;
+        public string StringWithContent = "Hello";
+
+        [Benchmark(Baseline = true)]
+        public bool ImperativeNull() => string.IsNullOrEmpty(NullString);
+
+        [Benchmark]
+        public bool ImperativeEmpty() => string.IsNullOrEmpty(EmptyString);
+
+        [Benchmark]
+        public bool ImperativeStringWithContent() => string.IsNullOrEmpty(StringWithContent);
+
+        [Benchmark]
+        public bool LightGuardClausesNull() => NullString.IsNullOrEmpty();
+
+        [Benchmark]
+        public bool LightGuardClausesEmpty() => EmptyString.IsNullOrEmpty();
+
+        [Benchmark]
+        public bool LightGuardClausesStringWithContent() => StringWithContent.IsNullOrEmpty();
+    }
+}
