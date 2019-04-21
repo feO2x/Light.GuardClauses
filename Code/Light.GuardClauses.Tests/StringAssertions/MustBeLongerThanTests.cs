e808be59870b4ee2fc262f17374803200b97f375
Kenny Pflug <kenny.pflug@live.de>
2019-04-21 07:36:42 +0200

    added string.MustBeLongerThan

---
+++ Code/Light.GuardClauses.Tests/StringAssertions/MustBeLongerThanTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Exceptions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.StringAssertions
+{
+    public static class MustBeLongerThanTests
+    {
+        [Theory]
+        [InlineData("Foo", 2)]
+        [InlineData("Gorge", 3)]
+        [InlineData("", -1)]
+        [InlineData("If you think this has a happy ending, you haven’t been paying attention.", 69)]
+        public static void LongerThan(string @string, int length) =>
+            @string.MustBeLongerThan(length).Should().BeSameAs(@string);
+
+        [Theory]
+        [InlineData("Bar", 3)]
+        [InlineData("Baz", 4)]
+        [InlineData("", 0)]
+        [InlineData("Hush, Hodor. No more Hodor-ing!", 100)]
+        public static void NotLongerThan(string @string, int length)
+        {
+            Action act = () => @string.MustBeLongerThan(length, nameof(@string));
+
+            act.Should().Throw<StringLengthException>()
+               .And.Message.Should().Contain($"string must be longer than {length}, but it actually has length {@string.Length}.");
+        }
+
+        [Fact]
+        public static void StringNull()
+        {
+            Action act = () => ((string) null).MustBeLongerThan(42);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public static void CustomException() =>
+            Test.CustomException("Foo",
+                                 5,
+                                 (s, l, exceptionFactory) => s.MustBeLongerThan(l, exceptionFactory));
+
+        [Fact]
+        public static void CustomMessage() =>
+            Test.CustomMessage<StringLengthException>(message => "Foo".MustBeLongerThan(10, message: message));
+    }
+}
--- Code/Light.GuardClauses/Exceptions/Throw.cs
+++ Code/Light.GuardClauses/Exceptions/Throw.cs
@@ -250,6 +250,13 @@ namespace Light.GuardClauses.Exceptions
             throw new StringLengthException(parameterName, message ?? $"{parameterName ?? "The string"} must have length {length}, but it actually has length {parameter.Length}.");
 
         /// <summary>
+        /// Throws the default <see cref="StringLengthException"/> indicating that a string is not longer than the given length, using the optional parameter name an message.
+        /// </summary>
+        [ContractAnnotation("=> halt")]
+        public static void StringNotLongerThan(string parameter, int length, string parameterName, string message) =>
+            throw new StringLengthException(parameterName, message ?? $"{parameterName ?? "The string"} must be longer than {length}, but it actually has length {parameter.Length}.");
+
+        /// <summary>
         /// Throws the default <see cref="ValuesNotEqualException" /> indicating that two values are not equal, using the optional parameter name and message.
         /// </summary>
         [ContractAnnotation("=> halt")]
