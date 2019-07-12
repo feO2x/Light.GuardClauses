b5e95c53e7bd0eb4e93e891ad79cc1c440382c97
Kenny Pflug <kenny.pflug@live.de>
2019-07-12 10:01:02 +0200

    added MustBeShorterThan for Span<T>

---
+++ Code/Light.GuardClauses.Tests/CollectionAssertions/SpanMustBeShorterThanTests.cs
@@ -0,0 +1,79 @@
+﻿#if NETCOREAPP2_2 || NET47
+using System;
+using FluentAssertions;
+using Light.GuardClauses.Exceptions;
+using Xunit;
+
+namespace Light.GuardClauses.Tests.CollectionAssertions
+{
+    public static class SpanMustBeShorterThanTests
+    {
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(0, 1)]
+        [InlineData(10, 18)]
+        public static void SpanShorter(int spanLength, int expectedLength)
+        {
+            var array = new int[10];
+            var span = new Span<int>(array, 0, spanLength);
+
+            var returnValue = span.MustBeShorterThan(expectedLength);
+
+            (returnValue == span).Should().BeTrue("the assertion returns a copy of the passed-in span");
+        }
+
+        [Theory]
+        [InlineData(10, 7)]
+        [InlineData(1, 0)]
+        [InlineData(5, 5)]
+        public static void SpanLongerOrEqual(int spanLength, int expectedLength)
+        {
+            Action act = () =>
+            {
+                var array = new char[15];
+                var span = new Span<char>(array, 0, spanLength);
+                span.MustBeShorterThan(expectedLength, nameof(span));
+            };
+
+            act.Should().Throw<InvalidCollectionCountException>()
+               .And.Message.Should().Contain($"span must be shorter than {expectedLength}, but it actually has length {spanLength}.");
+        }
+
+        [Fact]
+        public static void CustomException()
+        {
+            var exception = new Exception();
+
+            Action act = () =>
+            {
+                var span = new Span<byte>();
+                span.MustBeShorterThan(0, (s, l) => exception);
+            };
+
+            act.Should().Throw<Exception>().Which.Should().BeSameAs(exception);
+        }
+
+        [Fact]
+        public static void NoCustomException()
+        {
+            var span = new Span<char>();
+
+            var returnValue = span.MustBeShorterThan(5, null);
+
+            (returnValue == span).Should().BeTrue("the assertion returns a copy of the passed-in span");
+        }
+
+        [Fact]
+        public static void CustomMessage()
+        {
+            Action act = () =>
+            {
+                var span = new Span<byte>();
+                span.MustBeShorterThan(0, message: "Custom exception message");
+            };
+
+            act.Should().Throw<InvalidCollectionCountException>().And.Message.Should().Be("Custom exception message");
+        }
+    }
+}
+#endif
--- Code/Light.GuardClauses/Check.EnumerableAssertions.cs
+++ Code/Light.GuardClauses/Check.EnumerableAssertions.cs
@@ -427,7 +427,7 @@ namespace Light.GuardClauses
         /// Ensures that the span is longer than the specified length, or otherwise throws your custom exception.
         /// </summary>
         /// <param name="parameter">The span to be checked.</param>
-        /// <param name="length">The value that the span must be longer than.</param>
+        /// <param name="length">The length value that the span must be longer than.</param>
         /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> and <paramref name="length"/> are passed to it.</param>
         /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter"/> is shorter than or equal to <paramref name="length"/>.</exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -476,5 +476,38 @@ namespace Light.GuardClauses
                 Throw.CustomException(exceptionFactory, parameter, count);
             return parameter;
         }
+
+#if (NETSTANDARD2_0 || NET45)
+        /// <summary>
+        /// Ensures that the span is shorter than the specified length, or otherwise throws an <see cref="InvalidCollectionCountException"/>.
+        /// </summary>
+        /// <param name="parameter">The span to be checked.</param>
+        /// <param name="length">The length value that the span must be shorter than.</param>
+        /// <param name="parameterName">The name of the parameter (optional).</param>
+        /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
+        /// <exception cref="InvalidCollectionCountException">Thrown when <paramref name="parameter"/> is longer than or equal to <paramref name="length"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Span<T> MustBeShorterThan<T>(this Span<T> parameter, int length, string parameterName = null, string message = null)
+        {
+            if (parameter.Length >= length)
+                Throw.SpanMustBeShorterThan(parameter, length, parameterName, message);
+            return parameter;
+        }
+
+        /// <summary>
+        /// Ensures that the span is shorter than the specified length, or otherwise throws your custom exception.
+        /// </summary>
+        /// <param name="parameter">The span to be checked.</param>
+        /// <param name="length">The length value that the span must be shorter than.</param>
+        /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> and <paramref name="length"/> are passed to it.</param>
+        /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter"/> is longer than or equal to <paramref name="length"/>.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Span<T> MustBeShorterThan<T>(this Span<T> parameter, int length, SpanExceptionFactory<T, int> exceptionFactory)
+        {
+            if (parameter.Length >= length)
+                Throw.CustomSpanException(exceptionFactory, parameter, length);
+            return parameter;
+        }
+#endif
     }
 }
