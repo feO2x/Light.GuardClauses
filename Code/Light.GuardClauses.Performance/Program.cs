522053996ff4bc8e100ef8ecb4bd1d69dfb23c14
Kenny Pflug <kenny.pflug@live.de>
2019-12-11 11:29:10 +0100

    updated performance project to netcoreapp3.1 and net48

--- Code/Light.GuardClauses.Performance/Program.cs
+++ Code/Light.GuardClauses.Performance/Program.cs
@@ -1,31 +1,22 @@
-﻿using System;
-using System.Diagnostics;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Configs;
+﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace Light.GuardClauses.Performance
 {
-    [DisassemblyDiagnoser]
     public static class Program
     {
         private static IConfig DefaultConfiguration =>
             DefaultConfig
                .Instance
-               .With(Job.Clr, Job.Core)
+               .With(Job.Default.With(ClrRuntime.Net48))
+               .With(Job.Default.With(CoreRuntime.Core31))
                .With(MemoryDiagnoser.Default)
                .With(DisassemblyDiagnoser.Create(DisassemblyDiagnoserConfig.Asm));
 
-        public static void Main(string[] arguments)
-        {
-            Console.WriteLine($"Benchmarking started on {DateTimeOffset.Now}");
-            var stopWatch = Stopwatch.StartNew();
+        public static void Main(string[] arguments) =>
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(arguments, DefaultConfiguration);
-            stopWatch.Stop();
-            Console.WriteLine($"Benchmarking finished on {DateTimeOffset.Now}");
-            Console.WriteLine($"It took {stopWatch.Elapsed:g}");
-        }
     }
 }
