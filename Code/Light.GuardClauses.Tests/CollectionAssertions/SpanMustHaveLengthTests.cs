dc4e471a04e48e762ba5c103e2f35e9227d213d0
Kenny Pflug <kenny.pflug@live.de>
2021-12-03 19:53:12 +0100

    added CallerArgumentExpression test for Span MustHaveLength

--- Code/Light.GuardClauses.Tests/CollectionAssertions/SpanMustHaveLengthTests.cs
+++ Code/Light.GuardClauses.Tests/CollectionAssertions/SpanMustHaveLengthTests.cs
@@ -76,5 +76,18 @@ namespace Light.GuardClauses.Tests.CollectionAssertions
             act.Should().Throw<InvalidCollectionCountException>()
                .And.Message.Should().Contain("Foo");
         }
+
+        [Fact]
+        public static void CallerArgumentExpression()
+        {
+            var act = () =>
+            {
+                var mySpan = new Span<char>();
+                mySpan.MustHaveLength(10);
+            };
+
+            act.Should().Throw<InvalidCollectionCountException>()
+               .And.ParamName.Should().Be("mySpan");
+        }
     }
 }
