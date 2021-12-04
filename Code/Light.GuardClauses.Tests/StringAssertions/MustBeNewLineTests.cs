498dce4bbe5aac5bc9c3083ed134db886f3f48d9
Kenny Pflug <kenny.pflug@live.de>
2021-12-04 06:06:37 +0100

    added CallerArgumentExpression test for MustBeNewLine

--- Code/Light.GuardClauses.Tests/StringAssertions/MustBeNewLineTests.cs
+++ Code/Light.GuardClauses.Tests/StringAssertions/MustBeNewLineTests.cs
@@ -21,7 +21,7 @@ public static class MustBeNewLineTests
     {
         var stringValue = default(string);
 
-        Action act = () => stringValue.MustBeNewLine(nameof(stringValue));
+        var act = () => stringValue.MustBeNewLine(nameof(stringValue));
 
         act.Should().Throw<ArgumentNullException>()
            .And.ParamName.Should().Be(nameof(stringValue));
@@ -32,7 +32,7 @@ public static class MustBeNewLineTests
     [InlineData("")]
     public static void StringIsNotNewLine(string invalidString)
     {
-        Action act = () => invalidString.MustBeNewLine(nameof(invalidString));
+        var act = () => invalidString.MustBeNewLine(nameof(invalidString));
 
         act.Should().Throw<StringException>()
            .And.Message.Should().Contain($"{nameof(invalidString)} must be either \"\\n\" or \"\\r\\n\", but it actually is {invalidString.ToStringOrNull()}.");
@@ -56,5 +56,16 @@ public static class MustBeNewLineTests
             invalidString,
             (@string, exceptionFactory) => @string.MustBeNewLine(exceptionFactory!)
         );
+    }
+
+    [Fact]
+    public static void CallerArgumentExpression()
+    {
+        const string expression = "This is not a new line";
+
+        var act = () => expression.MustBeNewLine();
+
+        act.Should().Throw<StringException>()
+           .And.ParamName.Should().Be(nameof(expression));
     }
 }
