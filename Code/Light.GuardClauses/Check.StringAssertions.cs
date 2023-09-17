4c156d4cb6992d06ec2df46791d04ae0bd882429
Kenny Pflug <kenny.pflug@live.de>
2023-09-17 09:37:32 +0200

    feat: add IsTrimmedAtStart for string, ReadOnlySpan<char> and Span<char>
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.StringAssertions.cs
+++ Code/Light.GuardClauses/Check.StringAssertions.cs
@@ -1077,4 +1077,47 @@ public static partial class Check
         parameter.Length == 0 ||
         !parameter[0].IsWhiteSpace() &&
         !parameter[parameter.Length - 1].IsWhiteSpace();
+
+    /// <summary>
+    /// Checks if the specified string is trimmed at the start, i.e. it does not start with
+    /// white space characters. Inputting an empty sequence will return true.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="regardNullAsTrimmed">
+    /// The value indicating whether true or false should be returned from this method when the
+    /// <paramref name="parameter" /> is null. The default value is true.
+    /// </param>
+    /// <returns>
+    /// True if the <paramref name="parameter"/> is trimmed at the start, else false.
+    /// An empty string will result in true.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsTrimmedAtStart(this string? parameter, bool regardNullAsTrimmed = true) =>
+        parameter is null ? regardNullAsTrimmed : parameter.AsSpan().IsTrimmedAtStart();
+
+    /// <summary>
+    /// Checks if the specified character sequence is trimmed at the start, i.e. it does not start with
+    /// white space characters. Inputting an empty sequence will return true.
+    /// </summary>
+    /// <param name="parameter">The character sequence to be checked.</param>
+    /// <returns>
+    /// True if the <paramref name="parameter"/> is trimmed at the start, else false.
+    /// An empty sequence will result in true.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsTrimmedAtStart(this ReadOnlySpan<char> parameter) =>
+        parameter.Length == 0 || !parameter[0].IsWhiteSpace();
+    
+    /// <summary>
+    /// Checks if the specified character sequence is trimmed at the start, i.e. it does not start with
+    /// white space characters. Inputting an empty sequence will return true.
+    /// </summary>
+    /// <param name="parameter">The character sequence to be checked.</param>
+    /// <returns>
+    /// True if the <paramref name="parameter"/> is trimmed at the start, else false.
+    /// An empty sequence will result in true.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsTrimmedAtStart(this Span<char> parameter) =>
+        parameter.Length == 0 || !parameter[0].IsWhiteSpace();
 }
