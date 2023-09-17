c73e522b05734aff46f26e4c863d9d8e2d1415b5
Kenny Pflug <kenny.pflug@live.de>
2023-09-17 11:14:00 +0200

    feat: add IsTrimmedAtEnd from string, ReadOnlySpan<char> and Span<char>
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.StringAssertions.cs
+++ Code/Light.GuardClauses/Check.StringAssertions.cs
@@ -1120,4 +1120,47 @@ public static partial class Check
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsTrimmedAtStart(this Span<char> parameter) =>
         parameter.Length == 0 || !parameter[0].IsWhiteSpace();
+    
+    /// <summary>
+    /// Checks if the specified string is trimmed at the end, i.e. it does not end with
+    /// white space characters. Inputting an empty string will return true.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="regardNullAsTrimmed">
+    /// The value indicating whether true or false should be returned from this method when the
+    /// <paramref name="parameter" /> is null. The default value is true.
+    /// </param>
+    /// <returns>
+    /// True if the <paramref name="parameter"/> is trimmed at the start, else false.
+    /// An empty string will result in true.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsTrimmedAtEnd(this string? parameter, bool regardNullAsTrimmed = true) =>
+        parameter is null ? regardNullAsTrimmed : parameter.AsSpan().IsTrimmedAtEnd();
+
+    /// <summary>
+    /// Checks if the specified character span is trimmed at the end, i.e. it does not end with
+    /// white space characters. Inputting an empty span will return true.
+    /// </summary>
+    /// <param name="parameter">The character span to be checked.</param>
+    /// <returns>
+    /// True if the <paramref name="parameter"/> is trimmed at the end, else false.
+    /// An empty span will result in true.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsTrimmedAtEnd(this ReadOnlySpan<char> parameter) =>
+        parameter.Length == 0 || !parameter[parameter.Length - 1].IsWhiteSpace();
+    
+    /// <summary>
+    /// Checks if the specified character span is trimmed at the end, i.e. it does not end with
+    /// white space characters. Inputting an empty span will return true.
+    /// </summary>
+    /// <param name="parameter">The character span to be checked.</param>
+    /// <returns>
+    /// True if the <paramref name="parameter"/> is trimmed at the end, else false.
+    /// An empty span will result in true.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsTrimmedAtEnd(this Span<char> parameter) =>
+        parameter.Length == 0 || !parameter[parameter.Length - 1].IsWhiteSpace();
 }
