f4d034ee26554d73751faad4e3055563142b4e94
Kenny Pflug <kenny.pflug@live.de>
2024-11-28 06:50:10 +0100

    refactor: move MustNotBeDefault into own file
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.CommonAssertions.cs
+++ Code/Light.GuardClauses/Check.CommonAssertions.cs
@@ -13,62 +13,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the specified parameter is not the default value, or otherwise throws an <see cref="ArgumentNullException" />
-    /// for reference types, or an <see cref="ArgumentDefaultException" /> for value types.
-    /// </summary>
-    /// <param name="parameter">The value to be checked.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter" /> is a reference type and null.</exception>
-    /// <exception cref="ArgumentDefaultException">Thrown when <paramref name="parameter" /> is a value type and the default value.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static T MustNotBeDefault<T>([NotNull, ValidatedNotNull] this T parameter, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
-    {
-        if (default(T) is null)
-        {
-            if (parameter is null)
-                Throw.ArgumentNull(parameterName, message);
-            return parameter;
-        }
-
-        if (EqualityComparer<T>.Default.Equals(parameter, default!))
-            Throw.ArgumentDefault(parameterName, message);
-        
-        // If we end up here, we have a value type which cannot be null
-#pragma warning disable CS8777 // Parameter must have a non-null value when exiting.
-        return parameter;
-#pragma warning restore CS8777
-    }
-
-    /// <summary>
-    /// Ensures that the specified parameter is not the default value, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The value to be checked.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> is the default value.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull; exceptionFactory:null => halt")]
-    public static T MustNotBeDefault<T>([NotNull, ValidatedNotNull] this T parameter, Func<Exception> exceptionFactory)
-    {
-        if (default(T) is null)
-        {
-            if (parameter is null)
-                Throw.CustomException(exceptionFactory);
-            return parameter;
-        }
-
-        if (EqualityComparer<T>.Default.Equals(parameter, default!))
-            Throw.CustomException(exceptionFactory);
-        
-        // If we end up here, we have a value type which cannot be null
-#pragma warning disable CS8777 // Parameter must have a non-null value when exiting.
-        return parameter;
-#pragma warning restore CS8777
-
-    }
-
-    /// <summary>
     /// Ensures that the specified parameter is not null when <typeparamref name="T" /> is a reference type, or otherwise
     /// throws an <see cref="ArgumentNullException" />. PLEASE NOTICE: you should only use this assertion in generic contexts,
     /// use <see cref="MustNotBeNull{T}(T,string,string)" /> by default.
