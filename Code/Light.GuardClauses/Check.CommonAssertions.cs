27f770d8fb8e4ac45bbeee6ccb4705ae4384970d
Kenny Pflug <kenny.pflug@live.de>
2024-12-10 21:45:43 +0100

    refactor: move MustBeOfType method to its own file
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.CommonAssertions.cs
+++ Code/Light.GuardClauses/Check.CommonAssertions.cs
@@ -13,42 +13,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that <paramref name="parameter" /> can be cast to <typeparamref name="T" /> and returns the cast value, or otherwise throws a <see cref="TypeCastException" />.
-    /// </summary>
-    /// <param name="parameter">The value to be cast.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="TypeCastException">Thrown when <paramref name="parameter" /> cannot be cast to <typeparamref name="T" />.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter" /> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static T MustBeOfType<T>([NotNull, ValidatedNotNull, NoEnumeration] this object? parameter, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
-    {
-        if (parameter.MustNotBeNull(parameterName, message) is T castValue)
-            return castValue;
-
-        Throw.InvalidTypeCast(parameter, typeof(T), parameterName, message);
-        return default;
-    }
-
-    /// <summary>
-    /// Ensures that <paramref name="parameter" /> can be cast to <typeparamref name="T" /> and returns the cast value, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The value to be cast.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. The <paramref name="parameter" /> is passed to this delegate.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> cannot be cast to <typeparamref name="T" />.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull; exceptionFactory:null => halt")]
-    public static T MustBeOfType<T>([NotNull, ValidatedNotNull, NoEnumeration] this object? parameter, Func<object?, Exception> exceptionFactory)
-    {
-        if (parameter is T castValue)
-            return castValue;
-
-        Throw.CustomException(exceptionFactory, parameter);
-        return default;
-    }
-
-    /// <summary>
     /// Checks if the specified value is a valid enum value of its type. This is true when the specified value
     /// is one of the constants defined in the enum, or a valid flags combination when the enum type is marked
     /// with the <see cref="FlagsAttribute" />.
