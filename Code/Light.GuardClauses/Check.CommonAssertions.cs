95df065a90de2be0c9f277808373e911663e628b
Kenny Pflug <kenny.pflug@live.de>
2024-12-11 05:09:25 +0100

    refactor: move InvalidState from CommonAssertions into own file
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.CommonAssertions.cs
+++ Code/Light.GuardClauses/Check.CommonAssertions.cs
@@ -13,19 +13,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Checks if the specified <paramref name="condition" /> is true and throws an <see cref="InvalidStateException" /> in this case.
-    /// </summary>
-    /// <param name="condition">The condition to be checked. The exception is thrown when it is true.</param>
-    /// <param name="message">The message that will be passed to the <see cref="InvalidStateException" />.</param>
-    /// <exception cref="InvalidStateException">Thrown when <paramref name="condition" /> is true.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void InvalidState(bool condition, string? message = null)
-    {
-        if (condition)
-            Throw.InvalidState(message);
-    }
-
-    /// <summary>
     /// Ensures that the specified nullable has a value and returns it, or otherwise throws a <see cref="NullableHasNoValueException" />.
     /// </summary>
     /// <param name="parameter">The nullable to be checked.</param>
