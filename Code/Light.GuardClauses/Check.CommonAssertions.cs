047c85abc9500dba5020e3c684c7acaee80d1cbb
Kenny Pflug <kenny.pflug@live.de>
2024-12-11 05:11:27 +0100

    refactor: move MustHaveValue from CommonAssertions into own file
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.CommonAssertions.cs
+++ Code/Light.GuardClauses/Check.CommonAssertions.cs
@@ -13,38 +13,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the specified nullable has a value and returns it, or otherwise throws a <see cref="NullableHasNoValueException" />.
-    /// </summary>
-    /// <param name="parameter">The nullable to be checked.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="NullableHasNoValueException">Thrown when <paramref name="parameter" /> has no value.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T MustHaveValue<T>([NotNull, NoEnumeration] this T? parameter, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null) where T : struct
-    {
-        if (!parameter.HasValue)
-            Throw.NullableHasNoValue(parameterName, message);
-
-        return parameter.Value;
-    }
-
-    /// <summary>
-    /// Ensures that the specified nullable has a value and returns it, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The nullable to be checked.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception.</param>
-    /// <exception cref="NullableHasNoValueException">Thrown when <paramref name="parameter" /> has no value.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("exceptionFactory:null => halt")]
-    public static T MustHaveValue<T>([NotNull, NoEnumeration] this T? parameter, Func<Exception> exceptionFactory) where T : struct
-    {
-        if (!parameter.HasValue)
-            Throw.CustomException(exceptionFactory);
-
-        return parameter.Value;
-    }
-
-    /// <summary>
     /// Checks if <paramref name="parameter" /> and <paramref name="other" /> point to the same object.
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
---
+++ Code/Light.GuardClauses/Check.MustHaveValue.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Light.GuardClauses.Exceptions;
+using NotNullAttribute = System.Diagnostics.CodeAnalysis.NotNullAttribute;
+
+namespace Light.GuardClauses;
+
+public static partial class Check
+{
+    /// <summary>
+    /// Ensures that the specified nullable has a value and returns it, or otherwise throws a <see cref="NullableHasNoValueException" />.
+    /// </summary>
+    /// <param name="parameter">The nullable to be checked.</param>
+    /// <param name="parameterName">The name of the parameter (optional).</param>
+    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
+    /// <exception cref="NullableHasNoValueException">Thrown when <paramref name="parameter" /> has no value.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T MustHaveValue<T>(
+        [NotNull, NoEnumeration] this T? parameter,
+        [CallerArgumentExpression("parameter")] string? parameterName = null,
+        string? message = null
+    ) where T : struct
+    {
+        if (!parameter.HasValue)
+        {
+            Throw.NullableHasNoValue(parameterName, message);
+        }
+
+        return parameter.Value;
+    }
+
+    /// <summary>
+    /// Ensures that the specified nullable has a value and returns it, or otherwise throws your custom exception.
+    /// </summary>
+    /// <param name="parameter">The nullable to be checked.</param>
+    /// <param name="exceptionFactory">The delegate that creates your custom exception.</param>
+    /// <exception cref="NullableHasNoValueException">Thrown when <paramref name="parameter" /> has no value.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("exceptionFactory:null => halt")]
+    public static T MustHaveValue<T>([NotNull, NoEnumeration] this T? parameter, Func<Exception> exceptionFactory) where T : struct
+    {
+        if (!parameter.HasValue)
+        {
+            Throw.CustomException(exceptionFactory);
+        }
+
+        return parameter.Value;
+    }
+}
