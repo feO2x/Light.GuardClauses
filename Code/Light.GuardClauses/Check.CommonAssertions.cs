99421ca64b3ee873adb12cbf924fc9401bac78fd
Kenny Pflug <kenny.pflug@live.de>
2024-12-11 05:20:56 +0100

    refactor: move MustNotBe from CommonAssertions into own file
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.CommonAssertions.cs
+++ Code/Light.GuardClauses/Check.CommonAssertions.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
-using Light.GuardClauses.Exceptions;
 
 namespace Light.GuardClauses;
 
@@ -11,74 +8,6 @@ namespace Light.GuardClauses;
 /// </summary>
 public static partial class Check
 {
-    /// <summary>
-    /// Ensures that <paramref name="parameter" /> is not equal to <paramref name="other" /> using the default equality comparer, or otherwise throws a <see cref="ValuesEqualException" />.
-    /// </summary>
-    /// <param name="parameter">The first value to be compared.</param>
-    /// <param name="other">The other value to be compared.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="ValuesEqualException">Thrown when <paramref name="parameter" /> and <paramref name="other" /> are equal.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T MustNotBe<T>(this T parameter, T other, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(parameter, other))
-            Throw.ValuesEqual(parameter, other, parameterName, message);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that <paramref name="parameter" /> is not equal to <paramref name="other" /> using the default equality comparer, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The first value to be compared.</param>
-    /// <param name="other">The other value to be compared.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter" /> and <paramref name="other" /> are passed to this delegate.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> and <paramref name="other" /> are equal.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static T MustNotBe<T>(this T parameter, T other, Func<T, T, Exception> exceptionFactory)
-    {
-        if (EqualityComparer<T>.Default.Equals(parameter, other))
-            Throw.CustomException(exceptionFactory, parameter, other);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that <paramref name="parameter" /> is not equal to <paramref name="other" /> using the specified equality comparer, or otherwise throws a <see cref="ValuesEqualException" />.
-    /// </summary>
-    /// <param name="parameter">The first value to be compared.</param>
-    /// <param name="other">The other value to be compared.</param>
-    /// <param name="equalityComparer">The equality comparer used for comparing the two values.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="ValuesEqualException">Thrown when <paramref name="parameter" /> and <paramref name="other" /> are equal.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="equalityComparer" /> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("equalityComparer:null => halt")]
-    public static T MustNotBe<T>(this T parameter, T other, IEqualityComparer<T> equalityComparer, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
-    {
-        if (equalityComparer.MustNotBeNull(nameof(equalityComparer), message).Equals(parameter, other))
-            Throw.ValuesEqual(parameter, other, parameterName, message);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that <paramref name="parameter" /> is not equal to <paramref name="other" /> using the specified equality comparer, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The first value to be compared.</param>
-    /// <param name="other">The other value to be compared.</param>
-    /// <param name="equalityComparer">The equality comparer used for comparing the two values.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter" />, <paramref name="other" />, and <paramref name="equalityComparer" /> are passed to this delegate.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> and <paramref name="other" /> are equal, or when <paramref name="equalityComparer" /> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("equalityComparer:null => halt")]
-    public static T MustNotBe<T>(this T parameter, T other, IEqualityComparer<T> equalityComparer, Func<T, T, IEqualityComparer<T>, Exception> exceptionFactory)
-    {
-        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract - caller might have NRTs turned off
-        if (equalityComparer is null || equalityComparer.Equals(parameter, other))
-            Throw.CustomException(exceptionFactory, parameter, other, equalityComparer!);
-        return parameter;
-    }
-
     /// <summary>
     /// Checks if the specified value is approximately the same as the other value, using the given tolerance.
     /// </summary>
