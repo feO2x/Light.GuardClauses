8beaeb27dd536d9b8d33750a55dfce4f904c36dd
Kenny Pflug <kenny.pflug@live.de>
2024-12-26 20:14:29 +0100

    refactor: move MustNotContain assertions from Check.EnumerableAssertions.cs to Check.MustNotContain.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.EnumerableAssertions.cs
+++ Code/Light.GuardClauses/Check.EnumerableAssertions.cs
@@ -14,54 +14,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the collection does not contain the specified item, or otherwise throws an <see cref="ExistingItemException"/>.
-    /// </summary>
-    /// <param name="parameter">The collection to be checked.</param>
-    /// <param name="item">The item that must not be part of the collection.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="ExistingItemException">Thrown when <paramref name="parameter" /> contains <paramref name="item"/>.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter"/> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static TCollection MustNotContain<TCollection, TItem>([NotNull, ValidatedNotNull] this TCollection? parameter, TItem item, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null) where TCollection : class, IEnumerable<TItem>
-    {
-        if (parameter is ICollection<TItem> collection)
-        {
-            if (collection.Contains(item))
-                Throw.ExistingItem(parameter, item, parameterName, message);
-            return parameter;
-        }
-
-        if (parameter.MustNotBeNull(parameterName, message).Contains(item))
-            Throw.ExistingItem(parameter, item, parameterName, message);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that the collection does not contain the specified item, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The collection to be checked.</param>
-    /// <param name="item">The item that must not be part of the collection.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> and <paramref name="item"/> are passed to this delegate.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> contains <paramref name="item"/>.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static TCollection MustNotContain<TCollection, TItem>([NotNull, ValidatedNotNull] this TCollection? parameter, TItem item, Func<TCollection?, TItem, Exception> exceptionFactory) where TCollection : class, IEnumerable<TItem>
-    {
-        if (parameter is ICollection<TItem> collection)
-        {
-            if (collection.Contains(item))
-                Throw.CustomException(exceptionFactory, parameter, item);
-            return parameter;
-        }
-
-        if (parameter is null || parameter.Contains(item))
-            Throw.CustomException(exceptionFactory, parameter, item);
-        return parameter;
-    }
-
-    /// <summary>
     /// Checks if the given <paramref name="item" /> is one of the specified <paramref name="items" />.
     /// </summary>
     /// <param name="item">The item to be checked.</param>
