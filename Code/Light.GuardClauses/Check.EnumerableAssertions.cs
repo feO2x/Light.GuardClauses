21ec6fbb13ca2ce2332e2c08bb08fbed8e98bcb3
Kenny Pflug <kenny.pflug@live.de>
2024-12-26 20:08:51 +0100

    refactor: move MustNotBeNullOrEmpty assertions from Check.EnumerableAssertions.cs to Check.MustNotBeNullOrEmpty.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.EnumerableAssertions.cs
+++ Code/Light.GuardClauses/Check.EnumerableAssertions.cs
@@ -14,38 +14,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the collection is not null or empty, or otherwise throws an <see cref="EmptyCollectionException"/>.
-    /// </summary>
-    /// <param name="parameter">The collection to be checked.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="EmptyCollectionException">Thrown when <paramref name="parameter"/> has no items.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter"/> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static TCollection MustNotBeNullOrEmpty<TCollection>([NotNull, ValidatedNotNull] this TCollection? parameter, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null) where TCollection : class, IEnumerable
-    {
-        if (parameter.Count(parameterName, message) == 0)
-            Throw.EmptyCollection(parameterName, message);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that the collection is not null or empty, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The collection to be checked.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception.</param>
-    /// <exception cref="Exception">Thrown when <paramref name="parameter"/> has no items, or when <paramref name="parameter"/> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static TCollection MustNotBeNullOrEmpty<TCollection>([NotNull, ValidatedNotNull] this TCollection? parameter, Func<TCollection?, Exception> exceptionFactory) where TCollection : class, IEnumerable
-    {
-        if (parameter is null || parameter.Count() == 0)
-            Throw.CustomException(exceptionFactory, parameter);
-        return parameter;
-    }
-
-    /// <summary>
     /// Ensures that the collection contains the specified item, or otherwise throws a <see cref="MissingItemException"/>.
     /// </summary>
     /// <param name="parameter">The collection to be checked.</param>
