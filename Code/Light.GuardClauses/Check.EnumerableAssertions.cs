54c627959ec658acd872c57dbac5c7609fa42c9e
Kenny Pflug <kenny.pflug@live.de>
2024-12-26 20:16:41 +0100

    refactor: move IsOneOf method from Check.EnumerableAssertions.cs to Check.IsOneOf.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.EnumerableAssertions.cs
+++ Code/Light.GuardClauses/Check.EnumerableAssertions.cs
@@ -14,26 +14,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Checks if the given <paramref name="item" /> is one of the specified <paramref name="items" />.
-    /// </summary>
-    /// <param name="item">The item to be checked.</param>
-    /// <param name="items">The collection that might contain the <paramref name="item" />.</param>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="items" /> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("items:null => halt")]
-    // ReSharper disable once RedundantNullableFlowAttribute - the attribute has an effect, see Issue72NotNullAttribute tests
-    public static bool IsOneOf<TItem>(this TItem item, [NotNull, ValidatedNotNull] IEnumerable<TItem> items)
-    {
-        if (items is ICollection<TItem> collection)
-            return collection.Contains(item);
-
-        if (items is string @string && item is char character)
-            return @string.IndexOf(character) != -1;
-
-        return items.MustNotBeNull(nameof(items)).ContainsViaForeach(item);
-    }
-
-    /// <summary>
     /// Ensures that the value is one of the specified items, or otherwise throws a <see cref="ValueIsNotOneOfException"/>.
     /// </summary>
     /// <param name="parameter">The value to be checked.</param>
