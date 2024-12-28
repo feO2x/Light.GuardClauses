55b4a6b72ef48ce95fa03ca5ea00e93850741491
Kenny Pflug <kenny.pflug@live.de>
2024-12-28 19:51:23 +0100

    refactor: move MustBeShorterThanOrEqualTo methods from Check.StringAssertions.cs to Check.MustBeShorterThanOrEqualTo.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.StringAssertions.cs
+++ Code/Light.GuardClauses/Check.StringAssertions.cs
@@ -11,40 +11,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the string is shorter than or equal to the specified length, or otherwise throws a <see cref="StringLengthException"/>.
-    /// </summary>
-    /// <param name="parameter">The string to be checked.</param>
-    /// <param name="length">The length that the string must be shorter than or equal to.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="StringLengthException">Thrown when <paramref name="parameter"/> has a length greater than <paramref name="length"/>.</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter"/> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static string MustBeShorterThanOrEqualTo([NotNull, ValidatedNotNull] this string? parameter, int length, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
-    {
-        if (parameter.MustNotBeNull(parameterName, message).Length > length)
-            Throw.StringNotShorterThanOrEqualTo(parameter, length, parameterName, message);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that the string is shorter than or equal to the specified length, or otherwise throws your custom exception.
-    /// </summary>
-    /// <param name="parameter">The string to be checked.</param>
-    /// <param name="length">The length that the string must be shorter than or equal to.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> and <paramref name="length"/> are passed to this delegate.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter"/> is null or when it has a length greater than <paramref name="length"/>.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static string MustBeShorterThanOrEqualTo([NotNull, ValidatedNotNull] this string? parameter, int length, Func<string?, int, Exception> exceptionFactory)
-    {
-        if (parameter is null || parameter.Length > length)
-            Throw.CustomException(exceptionFactory, parameter, length);
-        return parameter;
-    }
-
-    /// <summary>
     /// Ensures that the string has the specified length, or otherwise throws a <see cref="StringLengthException"/>.
     /// </summary>
     /// <param name="parameter">The string to be checked.</param>
