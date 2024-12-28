e2c8e505928bd516b5d7b2fb81037a7127edb2a1
Kenny Pflug <kenny.pflug@live.de>
2024-12-28 20:09:56 +0100

    refactor: move IsTrimmed methods from Check.StringAssertions.cs to Check.IsTrimmed.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.StringAssertions.cs
+++ Code/Light.GuardClauses/Check.StringAssertions.cs
@@ -11,39 +11,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Checks if the specified string is trimmed, i.e. it does not start or end with
-    /// white space characters. Inputting an empty string will return true. When null is passed,
-    /// you can control the return value with <paramref name="regardNullAsTrimmed" /> which will
-    /// return true by default.
-    /// </summary>
-    /// <param name="parameter">The string to be checked.</param>
-    /// <param name="regardNullAsTrimmed">
-    /// The value indicating whether true or false should be returned from this method when the
-    /// <paramref name="parameter" /> is null. The default value is true.
-    /// </param>
-    /// <returns>
-    /// True if the <paramref name="parameter"/> is trimmed, else false. An empty string will result in true.
-    /// You can control the return value with <paramref name="regardNullAsTrimmed" /> when the
-    /// <paramref name="parameter" /> is null.
-    /// </returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static bool IsTrimmed(this string? parameter, bool regardNullAsTrimmed = true) =>
-        parameter is null ? regardNullAsTrimmed : parameter.AsSpan().IsTrimmed();
-
-    /// <summary>
-    /// Checks if the specified character span is trimmed, i.e. it does not start or end with
-    /// white space characters. Inputting an empty span will return true.
-    /// </summary>
-    /// <param name="parameter">The character span to be checked.</param>
-    /// <returns>True if the <paramref name="parameter"/> is trimmed, else false. An empty span will result in true.</returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static bool IsTrimmed(this ReadOnlySpan<char> parameter) =>
-        parameter.Length == 0 ||
-        !parameter[0].IsWhiteSpace() &&
-        !parameter[parameter.Length - 1].IsWhiteSpace();
-
-
-    /// <summary>
     /// Ensures that the string is not null and trimmed, or otherwise throws a <see cref="StringException"/>.
     /// Empty strings are regarded as trimmed.
     /// </summary>
