b29102d0a4966ff4ac9235415fe86c5a515138e8
Kenny Pflug <kenny.pflug@live.de>
2024-12-28 18:50:29 +0100

    refactor: move MustContain string validation methods from Check.StringAssertions.cs to Check.MustContain.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.MustContain.cs
+++ Code/Light.GuardClauses/Check.MustContain.cs
@@ -78,4 +78,122 @@ public static partial class Check
 
         return parameter;
     }
+
+    /// <summary>
+    /// Ensures that the string contains the specified substring, or otherwise throws a <see cref="SubstringException" />.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="value">The substring that must be part of <paramref name="parameter" />.</param>
+    /// <param name="parameterName">The name of the parameter (optional).</param>
+    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
+    /// <exception cref="SubstringException">Thrown when <paramref name="parameter" /> does not contain <paramref name="value" />.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter" /> or <paramref name="value" /> is null.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+    public static string MustContain(
+        [NotNull] [ValidatedNotNull] this string? parameter,
+        string? value,
+        [CallerArgumentExpression("parameter")] string? parameterName = null,
+        string? message = null
+    )
+    {
+        if (!parameter.MustNotBeNull(parameterName, message).Contains(value.MustNotBeNull(nameof(value), message)))
+        {
+            Throw.StringDoesNotContain(parameter, value, parameterName, message);
+        }
+
+        return parameter;
+    }
+
+    /// <summary>
+    /// Ensures that the string contains the specified value, or otherwise throws your custom exception.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="value">The substring that must be part of <paramref name="parameter" />.</param>
+    /// <param name="exceptionFactory">The delegate that creates you custom exception. <paramref name="parameter" /> and <paramref name="value" /> are passed to this delegate.</param>
+    /// <exception cref="Exception">
+    /// Your custom exception thrown when <paramref name="parameter" /> does not contain <paramref name="value" />,
+    /// or when <paramref name="parameter" /> is null,
+    /// or when <paramref name="value" /> is null.
+    /// </exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+    public static string MustContain(
+        [NotNull] [ValidatedNotNull] this string? parameter,
+        string value,
+        Func<string?, string, Exception> exceptionFactory
+    )
+    {
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract - caller might have NRTs turned off
+        if (parameter is null || value is null || !parameter.Contains(value))
+        {
+            Throw.CustomException(exceptionFactory, parameter, value!);
+        }
+
+        return parameter;
+    }
+
+    /// <summary>
+    /// Ensures that the string contains the specified value, or otherwise throws a <see cref="SubstringException" />.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="value">The substring that must be part of <paramref name="parameter" />.</param>
+    /// <param name="comparisonType">One of the enumeration values that specifies the rules for the search.</param>
+    /// <param name="parameterName">The name of the parameter (optional).</param>
+    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
+    /// <exception cref="SubstringException">Thrown when <paramref name="parameter" /> does not contain <paramref name="value" />.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter" /> or <paramref name="value" /> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="comparisonType" /> is not a valid <see cref="StringComparison" /> value.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+    public static string MustContain(
+        [NotNull] [ValidatedNotNull] this string? parameter,
+        string value,
+        StringComparison comparisonType,
+        [CallerArgumentExpression("parameter")] string? parameterName = null,
+        string? message = null
+    )
+    {
+        if (parameter.MustNotBeNull(parameterName, message).IndexOf(
+                value.MustNotBeNull(nameof(value), message),
+                comparisonType
+            ) <
+            0)
+        {
+            Throw.StringDoesNotContain(parameter, value, comparisonType, parameterName, message);
+        }
+
+        return parameter;
+    }
+
+    /// <summary>
+    /// Ensures that the string contains the specified value, or otherwise throws your custom exception.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="value">The substring that must be part of <paramref name="parameter" />.</param>
+    /// <param name="comparisonType">One of the enumeration values that specifies the rules for the search.</param>
+    /// <param name="exceptionFactory">The delegate that creates you custom exception. <paramref name="parameter" />, <paramref name="value" />, and <paramref name="comparisonType" /> are passed to this delegate.</param>
+    /// <exception cref="Exception">
+    /// Your custom exception thrown when <paramref name="parameter" /> does not contain <paramref name="value" />,
+    /// or when <paramref name="parameter" /> is null,
+    /// or when <paramref name="value" /> is null.
+    /// </exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="comparisonType" /> is not a valid <see cref="StringComparison" /> value.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+    public static string MustContain(
+        [NotNull] [ValidatedNotNull] this string? parameter,
+        string value,
+        StringComparison comparisonType,
+        Func<string?, string, StringComparison, Exception> exceptionFactory
+    )
+    {
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract - caller might have NRTs turned off
+        if (parameter is null || value is null || parameter.IndexOf(value, comparisonType) < 0)
+        {
+            Throw.CustomException(exceptionFactory, parameter, value!, comparisonType);
+        }
+
+        return parameter;
+    }
 }
