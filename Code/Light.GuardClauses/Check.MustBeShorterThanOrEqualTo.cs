5e4896ff6d8539baab14f61448a7ea988414a537
Kenny Pflug <kenny.pflug@live.de>
2024-12-28 08:05:00 +0100

    refactor: rename Check.EnumerableAssertions.cs to Check.MustBeShorterThanOrEqualTo.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.MustBeShorterThanOrEqualTo.cs
+++ Code/Light.GuardClauses/Check.MustBeShorterThanOrEqualTo.cs
@@ -1,31 +1,32 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
+using System;
 using System.Runtime.CompilerServices;
-using JetBrains.Annotations;
 using Light.GuardClauses.Exceptions;
-using Light.GuardClauses.FrameworkExtensions;
-using NotNullAttribute = System.Diagnostics.CodeAnalysis.NotNullAttribute;
 
 namespace Light.GuardClauses;
 
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the span is shorter than or equal to the specified length, or otherwise throws an <see cref="InvalidCollectionCountException"/>.
+    /// Ensures that the span is shorter than or equal to the specified length, or otherwise throws an <see cref="InvalidCollectionCountException" />.
     /// </summary>
     /// <param name="parameter">The span to be checked.</param>
     /// <param name="length">The length value that the span must be shorter than or equal to.</param>
     /// <param name="parameterName">The name of the parameter (optional).</param>
     /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="InvalidCollectionCountException">Thrown when <paramref name="parameter"/> is longer than <paramref name="length"/>.</exception>
+    /// <exception cref="InvalidCollectionCountException">Thrown when <paramref name="parameter" /> is longer than <paramref name="length" />.</exception>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Span<T> MustBeShorterThanOrEqualTo<T>(this Span<T> parameter, int length, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
+    public static Span<T> MustBeShorterThanOrEqualTo<T>(
+        this Span<T> parameter,
+        int length,
+        [CallerArgumentExpression("parameter")] string? parameterName = null,
+        string? message = null
+    )
     {
         if (parameter.Length > length)
+        {
             Throw.SpanMustBeShorterThanOrEqualTo(parameter, length, parameterName, message);
+        }
+
         return parameter;
     }
 
@@ -34,29 +35,44 @@ public static partial class Check
     /// </summary>
     /// <param name="parameter">The span to be checked.</param>
     /// <param name="length">The length value that the span must be shorter than or equal to.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> and <paramref name="length"/> are passed to it.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter"/> is longer than <paramref name="length"/>.</exception>
+    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter" /> and <paramref name="length" /> are passed to it.</param>
+    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> is longer than <paramref name="length" />.</exception>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Span<T> MustBeShorterThanOrEqualTo<T>(this Span<T> parameter, int length, SpanExceptionFactory<T, int> exceptionFactory)
+    public static Span<T> MustBeShorterThanOrEqualTo<T>(
+        this Span<T> parameter,
+        int length,
+        SpanExceptionFactory<T, int> exceptionFactory
+    )
     {
         if (parameter.Length > length)
+        {
             Throw.CustomSpanException(exceptionFactory, parameter, length);
+        }
+
         return parameter;
     }
 
     /// <summary>
-    /// Ensures that the span is shorter than or equal to the specified length, or otherwise throws an <see cref="InvalidCollectionCountException"/>.
+    /// Ensures that the span is shorter than or equal to the specified length, or otherwise throws an <see cref="InvalidCollectionCountException" />.
     /// </summary>
     /// <param name="parameter">The span to be checked.</param>
     /// <param name="length">The length value that the span must be shorter than or equal to.</param>
     /// <param name="parameterName">The name of the parameter (optional).</param>
     /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="InvalidCollectionCountException">Thrown when <paramref name="parameter"/> is longer than <paramref name="length"/>.</exception>
+    /// <exception cref="InvalidCollectionCountException">Thrown when <paramref name="parameter" /> is longer than <paramref name="length" />.</exception>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ReadOnlySpan<T> MustBeShorterThanOrEqualTo<T>(this ReadOnlySpan<T> parameter, int length, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
+    public static ReadOnlySpan<T> MustBeShorterThanOrEqualTo<T>(
+        this ReadOnlySpan<T> parameter,
+        int length,
+        [CallerArgumentExpression("parameter")] string? parameterName = null,
+        string? message = null
+    )
     {
         if (parameter.Length > length)
+        {
             Throw.SpanMustBeShorterThanOrEqualTo(parameter, length, parameterName, message);
+        }
+
         return parameter;
     }
 
@@ -65,13 +81,20 @@ public static partial class Check
     /// </summary>
     /// <param name="parameter">The span to be checked.</param>
     /// <param name="length">The length value that the span must be shorter than or equal to.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> and <paramref name="length"/> are passed to it.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter"/> is longer than <paramref name="length"/>.</exception>
+    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter" /> and <paramref name="length" /> are passed to it.</param>
+    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> is longer than <paramref name="length" />.</exception>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static ReadOnlySpan<T> MustBeShorterThanOrEqualTo<T>(this ReadOnlySpan<T> parameter, int length, ReadOnlySpanExceptionFactory<T, int> exceptionFactory)
+    public static ReadOnlySpan<T> MustBeShorterThanOrEqualTo<T>(
+        this ReadOnlySpan<T> parameter,
+        int length,
+        ReadOnlySpanExceptionFactory<T, int> exceptionFactory
+    )
     {
         if (parameter.Length > length)
+        {
             Throw.CustomSpanException(exceptionFactory, parameter, length);
+        }
+
         return parameter;
     }
 }
