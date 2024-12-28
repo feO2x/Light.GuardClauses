c1a34cf235dbdde8e2a85d54040e041bf05ac969
Kenny Pflug <kenny.pflug@live.de>
2024-12-28 19:26:51 +0100

    refactor: add IsEmailAddress validation methods and remove from Check.StringAssertions.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses/Check.StringAssertions.cs
+++ Code/Light.GuardClauses/Check.StringAssertions.cs
@@ -11,27 +11,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Checks if the specified string is an email address using the default email regular expression
-    /// defined in <see cref="RegularExpressions.EmailRegex"/>.
-    /// </summary>
-    /// <param name="emailAddress">The string to be checked if it is an email address.</param>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("emailAddress:null => false")]
-    public static bool IsEmailAddress([NotNullWhen(true)] this string? emailAddress) =>
-        emailAddress != null && RegularExpressions.EmailRegex.IsMatch(emailAddress);
-
-    /// <summary>
-    /// Checks if the specified string is an email address using the provided regular expression for validation.
-    /// </summary>
-    /// <param name="emailAddress">The string to be checked.</param>
-    /// <param name="emailAddressPattern">The regular expression that determines whether the input string is an email address.</param>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern"/> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("emailAddress:null => false; emailAddressPattern:null => halt")]
-    public static bool IsEmailAddress([NotNullWhen(true)] this string? emailAddress, Regex emailAddressPattern) =>
-        emailAddress != null && emailAddressPattern.MustNotBeNull(nameof(emailAddressPattern)).IsMatch(emailAddress);
-
-    /// <summary>
     /// Ensures that the string is a valid email address using the default email regular expression
     /// defined in <see cref="RegularExpressions.EmailRegex"/>, or otherwise throws an <see cref="InvalidEmailAddressException"/>.
     /// </summary>
