66a0de9240712c63f9d55a39e902c678bc967689
Kenny Pflug <kenny.pflug@live.de>
2024-12-28 20:07:44 +0100

    refactor: move MustBeNewLine methods from Check.StringAssertions.cs to Check.MustBeNewLine.cs
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

---
+++ Code/Light.GuardClauses/Check.MustBeNewLine.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+using Light.GuardClauses.Exceptions;
+using NotNullAttribute = System.Diagnostics.CodeAnalysis.NotNullAttribute;
+
+namespace Light.GuardClauses;
+
+public static partial class Check
+{
+    /// <summary>
+    /// Ensures that the string is either "\n" or "\r\n", or otherwise throws a <see cref="StringException" />. This is done independently of the current value of <see cref="Environment.NewLine" />.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="parameterName">The name of the parameter (optional).</param>
+    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
+    /// <exception cref="StringException">Thrown when <paramref name="parameter" /> is not equal to "\n" or "\r\n".</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter" /> is null.</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+    public static string MustBeNewLine(
+        [NotNull] [ValidatedNotNull] this string? parameter,
+        [CallerArgumentExpression("parameter")] string? parameterName = null,
+        string? message = null
+    )
+    {
+        if (!parameter.MustNotBeNull(parameterName, message).IsNewLine())
+        {
+            Throw.NotNewLine(parameter, parameterName, message);
+        }
+
+        return parameter;
+    }
+
+    /// <summary>
+    /// Ensures that the string is either "\n" or "\r\n", or otherwise throws your custom exception. This is done independently of the current value of <see cref="Environment.NewLine" />.
+    /// </summary>
+    /// <param name="parameter">The string to be checked.</param>
+    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter" /> is passed to this delegate.</param>
+    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter" /> is not equal to "\n" or "\r\n".</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
+    public static string MustBeNewLine(
+        [NotNull] [ValidatedNotNull] this string? parameter,
+        Func<string?, Exception> exceptionFactory
+    )
+    {
+        if (!parameter.IsNewLine())
+        {
+            Throw.CustomException(exceptionFactory, parameter);
+        }
+
+        return parameter;
+    }
+}
--- Code/Light.GuardClauses/Check.StringAssertions.cs
+++ Code/Light.GuardClauses/Check.StringAssertions.cs
@@ -11,38 +11,6 @@ namespace Light.GuardClauses;
 public static partial class Check
 {
     /// <summary>
-    /// Ensures that the string is either "\n" or "\r\n", or otherwise throws a <see cref="StringException"/>. This is done independently of the current value of <see cref="Environment.NewLine"/>.
-    /// </summary>
-    /// <param name="parameter">The string to be checked.</param>
-    /// <param name="parameterName">The name of the parameter (optional).</param>
-    /// <param name="message">The message that will be passed to the resulting exception (optional).</param>
-    /// <exception cref="StringException">Thrown when <paramref name="parameter"/> is not equal to "\n" or "\r\n".</exception>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="parameter"/> is null.</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static string MustBeNewLine([NotNull, ValidatedNotNull] this string? parameter, [CallerArgumentExpression("parameter")] string? parameterName = null, string? message = null)
-    {
-        if (!parameter.MustNotBeNull(parameterName, message).IsNewLine())
-            Throw.NotNewLine(parameter, parameterName, message);
-        return parameter;
-    }
-
-    /// <summary>
-    /// Ensures that the string is either "\n" or "\r\n", or otherwise throws your custom exception. This is done independently of the current value of <see cref="Environment.NewLine"/>.
-    /// </summary>
-    /// <param name="parameter">The string to be checked.</param>
-    /// <param name="exceptionFactory">The delegate that creates your custom exception. <paramref name="parameter"/> is passed to this delegate.</param>
-    /// <exception cref="Exception">Your custom exception thrown when <paramref name="parameter"/> is not equal to "\n" or "\r\n".</exception>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    [ContractAnnotation("parameter:null => halt; parameter:notnull => notnull")]
-    public static string MustBeNewLine([NotNull, ValidatedNotNull] this string? parameter, Func<string?, Exception> exceptionFactory)
-    {
-        if (!parameter.IsNewLine())
-            Throw.CustomException(exceptionFactory, parameter);
-        return parameter;
-    }
-
-    /// <summary>
     /// Checks if the specified string is trimmed, i.e. it does not start or end with
     /// white space characters. Inputting an empty string will return true. When null is passed,
     /// you can control the return value with <paramref name="regardNullAsTrimmed" /> which will
