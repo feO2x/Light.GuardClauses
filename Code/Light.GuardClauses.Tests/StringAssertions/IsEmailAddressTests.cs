08adfbcaaf314f2e701a87280a8aed7450cf8587
Kenny Pflug <kenny.pflug@live.de>
2025-03-02 17:51:47 +0100

    feat: add IsEmailAddress overloads for Span<T> and Memory<T>
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- Code/Light.GuardClauses.Tests/StringAssertions/IsEmailAddressTests.cs
+++ Code/Light.GuardClauses.Tests/StringAssertions/IsEmailAddressTests.cs
@@ -1,27 +1,33 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Xunit;
 
 namespace Light.GuardClauses.Tests.StringAssertions;
 
 public sealed class IsEmailAddressTests
 {
+    public static readonly TheoryData<string> InvalidEmailAddresses =
+    [
+        null,
+        "plainaddress",
+        "#@%^%#$@#$@#.com",
+        "@domain.com",
+        "Joe Smith <email@domain.com>",
+        "email.domain.com",
+        "email@domain@domain.com",
+        ".email@domain.com",
+        "email.@domain.com",
+        "email..email@domain.com",
+        "email@domain.com (Joe Smith)",
+        "email@domain",
+        "email@-domain.com",
+        "email@111.222.333.44444",
+        "email@domain..com",
+        "email@256.256.256.256",
+    ];
+
     [Theory]
-    [InlineData(null)]
-    [InlineData("plainaddress")]
-    [InlineData("#@%^%#$@#$@#.com")]
-    [InlineData("@domain.com")]
-    [InlineData("Joe Smith <email@domain.com>")]
-    [InlineData("email.domain.com")]
-    [InlineData("email@domain@domain.com")]
-    [InlineData(".email@domain.com")]
-    [InlineData("email.@domain.com")]
-    [InlineData("email..email@domain.com")]
-    [InlineData("email@domain.com (Joe Smith)")]
-    [InlineData("email@domain")]
-    [InlineData("email@-domain.com")]
-    [InlineData("email@111.222.333.44444")]
-    [InlineData("email@domain..com")]
-    [InlineData("email@256.256.256.256")] // Invalid IP (values > 255)
+    [MemberData(nameof(InvalidEmailAddresses))]
     public void IsNotValidEmailAddress(string email)
     {
         var isValid = email.IsEmailAddress();
@@ -29,29 +35,116 @@ public sealed class IsEmailAddressTests
         isValid.Should().BeFalse();
     }
 
+    public static readonly TheoryData<string> ValidEmailAddresses =
+    [
+        "email@domain.com",
+        "firstname.lastname@domain.com",
+        "email@subdomain.domain.com",
+        "firstname+lastname@domain.com",
+        "email@123.123.123.123",
+        "1234567890@domain.com",
+        "email@domain-one.com",
+        "_______@domain.com",
+        "email@domain.name",
+        "email@domain.co.jp",
+        "firstname-lastname@domain.com",
+        "email@domain.museum", // Long TLD (>4 chars)
+        "email@domain.travel", // Another long TLD
+        "email@domain.photography", // Even longer TLD
+        "email@[IPv6:2001:db8::1]", // IPv6 format
+        "\"quoted\"@domain.com", // Quoted local part
+        "user.name+tag+sorting@example.com", // Gmail-style + addressing
+        "あいうえお@domain.com", // Unicode character test
+    ];
+
     [Theory]
-    [InlineData("email@domain.com")]
-    [InlineData("firstname.lastname@domain.com")]
-    [InlineData("email@subdomain.domain.com")]
-    [InlineData("firstname+lastname@domain.com")]
-    [InlineData("email@123.123.123.123")]
-    [InlineData("1234567890@domain.com")]
-    [InlineData("email@domain-one.com")]
-    [InlineData("_______@domain.com")]
-    [InlineData("email@domain.name")]
-    [InlineData("email@domain.co.jp")]
-    [InlineData("firstname-lastname@domain.com")]
-    [InlineData("email@domain.museum")] // Long TLD (>4 chars)
-    [InlineData("email@domain.travel")] // Another long TLD
-    [InlineData("email@domain.photography")] // Even longer TLD
-    [InlineData("email@[IPv6:2001:db8::1]")] // IPv6 format
-    [InlineData("\"quoted\"@domain.com")] // Quoted local part
-    [InlineData("user.name+tag+sorting@example.com")] // Gmail-style + addressing
-    [InlineData("あいうえお@domain.com")] // Unicode character test
+    [MemberData(nameof(ValidEmailAddresses))]
     public void IsValidEmailAddress(string email)
     {
         var isValid = email.IsEmailAddress();
 
         isValid.Should().BeTrue();
     }
+
+#if NET8_0
+    [Theory]
+    [MemberData(nameof(InvalidEmailAddresses))]
+    public void IsNotValidEmailAddress_ReadOnlySpan(string email)
+    {
+        var span = new ReadOnlySpan<char>(email?.ToCharArray() ?? []);
+        var isValid = span.IsEmailAddress();
+
+        isValid.Should().BeFalse();
+    }
+
+    [Theory]
+    [MemberData(nameof(ValidEmailAddresses))]
+    public void IsValidEmailAddress_ReadOnlySpan(string email)
+    {
+        var span = email.AsSpan();
+        var isValid = span.IsEmailAddress();
+
+        isValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidEmailAddresses))]
+    public void IsNotValidEmailAddress_Span(string email)
+    {
+        var span = new Span<char>(email?.ToCharArray() ?? []);
+        var isValid = span.IsEmailAddress();
+
+        isValid.Should().BeFalse();
+    }
+
+    [Theory]
+    [MemberData(nameof(ValidEmailAddresses))]
+    public void IsValidEmailAddress_Span(string email)
+    {
+        var span = new Span<char>(email.ToCharArray());
+        var isValid = span.IsEmailAddress();
+
+        isValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidEmailAddresses))]
+    public void IsNotValidEmailAddress_Memory(string email)
+    {
+        var memory = email?.ToCharArray().AsMemory() ?? Memory<char>.Empty;
+        var isValid = memory.IsEmailAddress();
+
+        isValid.Should().BeFalse();
+    }
+
+    [Theory]
+    [MemberData(nameof(ValidEmailAddresses))]
+    public void IsValidEmailAddress_Memory(string email)
+    {
+        var memory = email.ToCharArray().AsMemory();
+        var isValid = memory.IsEmailAddress();
+
+        isValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidEmailAddresses))]
+    public void IsNotValidEmailAddress_ReadOnlyMemory(string email)
+    {
+        var memory = new ReadOnlyMemory<char>(email?.ToCharArray() ?? []);
+        var isValid = memory.IsEmailAddress();
+
+        isValid.Should().BeFalse();
+    }
+
+    [Theory]
+    [MemberData(nameof(ValidEmailAddresses))]
+    public void IsValidEmailAddress_ReadOnlyMemory(string email)
+    {
+        var memory = new ReadOnlyMemory<char>(email.ToCharArray());
+        var isValid = memory.IsEmailAddress();
+
+        isValid.Should().BeTrue();
+    }
+#endif
 }
--- Code/Light.GuardClauses/Check.IsEmailAddress.cs
+++ Code/Light.GuardClauses/Check.IsEmailAddress.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 using JetBrains.Annotations;
-using System.Runtime.CompilerServices;
 
 namespace Light.GuardClauses;
 
@@ -10,7 +10,7 @@ public static partial class Check
 {
     /// <summary>
     /// Checks if the specified string is an email address using the default email regular expression
-    /// defined in <see cref="RegularExpressions.EmailRegex"/>.
+    /// defined in <see cref="RegularExpressions.EmailRegex" />.
     /// </summary>
     /// <param name="emailAddress">The string to be checked if it is an email address.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -23,9 +23,79 @@ public static partial class Check
     /// </summary>
     /// <param name="emailAddress">The string to be checked.</param>
     /// <param name="emailAddressPattern">The regular expression that determines whether the input string is an email address.</param>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern"/> is null.</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern" /> is null.</exception>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [ContractAnnotation("emailAddress:null => false; emailAddressPattern:null => halt")]
     public static bool IsEmailAddress([NotNullWhen(true)] this string? emailAddress, Regex emailAddressPattern) =>
         emailAddress != null && emailAddressPattern.MustNotBeNull(nameof(emailAddressPattern)).IsMatch(emailAddress);
+
+#if NET8_0
+    /// <summary>
+    /// Checks if the specified span is an email address using the default email regular expression
+    /// defined in <see cref="RegularExpressions.EmailRegex" />.
+    /// </summary>
+    /// <param name="emailAddress">The span to be checked.</param>
+    public static bool IsEmailAddress(this Span<char> emailAddress) =>
+        RegularExpressions.EmailRegex.IsMatch(emailAddress);
+
+    /// <summary>
+    /// Checks if the specified span is an email address using the provided regular expression for validation.
+    /// </summary>
+    /// <param name="emailAddress">The span to be checked.</param>
+    /// <param name="emailAddressPattern">The regular expression that determines whether the input string is an email address.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern" /> is null.</exception>
+    public static bool IsEmailAddress(this Span<char> emailAddress, Regex emailAddressPattern) =>
+        emailAddressPattern.MustNotBeNull(nameof(emailAddressPattern)).IsMatch(emailAddress);
+
+    /// <summary>
+    /// Checks if the specified span is an email address using the default email regular expression
+    /// defined in <see cref="RegularExpressions.EmailRegex" />.
+    /// </summary>
+    /// <param name="emailAddress">The span to be checked.</param>
+    public static bool IsEmailAddress(this ReadOnlySpan<char> emailAddress) =>
+        RegularExpressions.EmailRegex.IsMatch(emailAddress);
+
+    /// <summary>
+    /// Checks if the specified span is an email address using the provided regular expression for validation.
+    /// </summary>
+    /// <param name="emailAddress">The span to be checked.</param>
+    /// <param name="emailAddressPattern">The regular expression that determines whether the input string is an email address.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern" /> is null.</exception>
+    public static bool IsEmailAddress(this ReadOnlySpan<char> emailAddress, Regex emailAddressPattern) =>
+        emailAddressPattern.MustNotBeNull(nameof(emailAddressPattern)).IsMatch(emailAddress);
+    
+    /// <summary>
+    /// Checks if the specified character memory is an email address using the default email regular expression
+    /// defined in <see cref="RegularExpressions.EmailRegex" />.
+    /// </summary>
+    /// <param name="emailAddress">The character memory to be checked.</param>
+    public static bool IsEmailAddress(this Memory<char> emailAddress) =>
+        RegularExpressions.EmailRegex.IsMatch(emailAddress.Span);
+    
+    /// <summary>
+    /// Checks if the specified character memory is an email address using the provided regular expression for validation.
+    /// </summary>
+    /// <param name="emailAddress">The character memory to be checked.</param>
+    /// <param name="emailAddressPattern">The regular expression that determines whether the input string is an email address.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern" /> is null.</exception>
+    public static bool IsEmailAddress(this Memory<char> emailAddress, Regex emailAddressPattern) =>
+        emailAddressPattern.MustNotBeNull(nameof(emailAddressPattern)).IsMatch(emailAddress.Span);
+    
+    /// <summary>
+    /// Checks if the specified character memory is an email address using the default email regular expression
+    /// defined in <see cref="RegularExpressions.EmailRegex" />.
+    /// </summary>
+    /// <param name="emailAddress">The character memory to be checked.</param>
+    public static bool IsEmailAddress(this ReadOnlyMemory<char> emailAddress) =>
+        RegularExpressions.EmailRegex.IsMatch(emailAddress.Span);
+    
+    /// <summary>
+    /// Checks if the specified character memory is an email address using the provided regular expression for validation.
+    /// </summary>
+    /// <param name="emailAddress">The character memory to be checked.</param>
+    /// <param name="emailAddressPattern">The regular expression that determines whether the input string is an email address.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="emailAddressPattern" /> is null.</exception>
+    public static bool IsEmailAddress(this ReadOnlyMemory<char> emailAddress, Regex emailAddressPattern) =>
+        emailAddressPattern.MustNotBeNull(nameof(emailAddressPattern)).IsMatch(emailAddress.Span);
+#endif
 }
